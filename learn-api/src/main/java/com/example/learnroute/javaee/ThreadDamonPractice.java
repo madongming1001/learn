a7a13fca1bf14f637fde1f123c624124585a5814
madongming <1484178114@qq.com>
2021-11-06 16:35:14 +0800

    守护线程代码提交

---
+++ learn-api/src/main/java/com/example/learnroute/javaee/ThreadDamonPractice.java
@@ -0,0 +1,23 @@
+package com.example.learnroute.javaee;
+
+public class ThreadDamonPractice {
+    public static void main(String[] args) {
+        Thread mainThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("我是一个用户线程");
+            }
+        });
+        Thread daemonThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(mainThread.isAlive()){
+                    System.out.println("我是一个守护线程");
+                }
+            }
+        });
+        daemonThread.setDaemon(true);
+        mainThread.start();
+        daemonThread.start();
+    }
+}
