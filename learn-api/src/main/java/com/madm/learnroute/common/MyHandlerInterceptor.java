ad64371e431ede291bc989cbb030b4ab4ea38a9d
madongming <1484178114@qq.com>
2021-11-12 13:06:09 +0800

    自定义注解拦截

--- learn-api/src/main/java/com/madm/learnroute/common/MyHandlerInterceptor.java
+++ learn-api/src/main/java/com/madm/learnroute/common/MyHandlerInterceptor.java
@@ -1,16 +1,30 @@
 package com.madm.learnroute.common;
 
 
+import com.madm.learnroute.auth.UserAuthenticate;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.Objects;
 
 public class MyHandlerInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle方法运行");
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+        UserAuthenticate userAuthenticate = method.getAnnotation(UserAuthenticate.class);
+        //如果没有加注解则userAuthenticate为null
+        if (Objects.nonNull(userAuthenticate)) {
+            System.out.println("传入了userAuthenticate注解");
+        }else{
+            System.out.println("没有传入userAuthenticate注解");
+        }
         return true;
     }
 
--- learn-api/src/main/java/com/madm/learnroute/controller/PracticeController.java
+++ learn-api/src/main/java/com/madm/learnroute/controller/PracticeController.java
@@ -1,5 +1,6 @@
 package com.madm.learnroute.controller;
 
+import com.madm.learnroute.auth.UserAuthenticate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/practice")
 public class PracticeController {
 
+    @UserAuthenticate
     @GetMapping(value="/practiceAccess")
     public String practiceAccess(){
+        System.out.println("走到了practiceAccess方法");
         return "访问成功";
     }
 
