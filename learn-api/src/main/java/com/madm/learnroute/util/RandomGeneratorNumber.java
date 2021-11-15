68e78e932fcefbc776a0ca4004e54e007085e1f3
madongming <1484178114@qq.com>
2021-11-15 22:33:16 +0800

    大文件排序代码

---
+++ learn-api/src/main/java/com/madm/learnroute/util/RandomGeneratorNumber.java
@@ -0,0 +1,18 @@
+package com.madm.learnroute.util;
+
+public class RandomGeneratorNumber {
+
+
+    public static int[] createArrayOfSize(int size) {
+        int[] result = new int[size];
+        int max  = 0;
+        for (int i = 0; i < size; i++) {
+            result[i] = (int)(Math.random()*size) + 1;
+            if(max < result[i]){
+                max = result[i];
+            }
+        }
+        System.out.println(max);
+        return result;
+    }
+}
