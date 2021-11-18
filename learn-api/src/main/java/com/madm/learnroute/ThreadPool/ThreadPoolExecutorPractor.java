b17af1b4f07e29cf88a4c28d17bbc26914a03892
madm <madongming1001@163.com>
2021-11-19 00:13:44 +0800

    找到第一个出现一个字符的算法添加

--- learn-api/src/main/java/com/madm/learnroute/ThreadPool/ThreadPoolExecutorPractor.java
+++ learn-api/src/main/java/com/madm/learnroute/ThreadPool/ThreadPoolExecutorPractor.java
@@ -1,4 +1,35 @@
 package com.madm.learnroute.ThreadPool;
 
+import java.util.concurrent.*;
+
 public class ThreadPoolExecutorPractor {
+    public static void main(String[] args) {
+        // 线程池的对应状态：
+        // RUNNING
+        // SHUTDOWN
+        // STOP
+        // TIDYING 整理
+        // TERMINATED
+        // private static final int CAPACITY   = (1 << COUNT_BITS) - 1;
+        // private static int runStateOf(int c)     { return c & ~CAPACITY; }
+        // private static int workerCountOf(int c)  { return c & CAPACITY; }
+        // private static int ctlOf(int rs, int wc) { return rs | wc; }
+        // DiscardPolicy
+        // CallerRunPolicy
+        // AbortPolicy
+        // DiscardOldestPolicy
+        // DiscardPolicy 第五种什么也没做
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 5, 1000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(1), Executors.defaultThreadFactory(), new RejectedExecutionHandler() {
+            @Override
+            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+
+            }
+        });
+        threadPoolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Long.valueOf("1001010001010101",2));
+            }
+        });
+    }
 }
