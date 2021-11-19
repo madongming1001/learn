14c4ef52a50e1a5b56f6658e676a046cb571940e
madm <madongming1001@163.com>
2021-11-19 22:22:16 +0800

    thread测试类提交

---
+++ learn-api/src/main/java/com/madm/learnroute/threadbase/LockSupportTest.java
@@ -0,0 +1,26 @@
+package com.tuling.jucdemo.threadbase;
+
+import java.util.concurrent.locks.LockSupport;
+
+public class LockSupportTest {
+
+    public static void main(String[] args) {
+        Thread parkThread = new Thread(new ParkThread());
+        parkThread.start();
+
+        System.out.println("唤醒parkThread");
+        //为指定线程parkThread提供“许可”
+        LockSupport.unpark(parkThread);
+    }
+
+    static class ParkThread implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("ParkThread开始执行");
+            // 等待“许可”
+            LockSupport.park();
+            System.out.println("ParkThread执行完成");
+        }
+    }
+}
---
+++ learn-api/src/main/java/com/madm/learnroute/threadbase/ThreadInterruptTest.java
@@ -0,0 +1,45 @@
+package com.tuling.jucdemo.threadbase;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author  Fox
+ * 中断机制
+ */
+public class ThreadInterruptTest {
+
+    static int i = 0;
+
+    public static void main(String[] args)  {
+        System.out.println("begin");
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public  void run() {
+                while (true) {
+                    i++;
+                    System.out.println(i);
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    //Thread.interrupted()  清除中断标志位
+                    //Thread.currentThread().isInterrupted() 不会清除中断标志位
+                    if (Thread.interrupted()  ) {
+                        System.out.println("=========");
+                    }
+                    if(i==10){
+                        break;
+                    }
+
+                }
+            }
+        });
+
+        t1.start();
+        //不会停止线程t1,只会设置一个中断标志位 flag=true
+        t1.interrupt();
+
+    }
+}
