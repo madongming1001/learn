7781d05bcc146558d76fa05d443f04d921df4bdd
madongming <madongming1001@163.com>
2021-11-25 15:37:05 +0800

    空间局限性测试

---
+++ learn-api/src/main/java/com/madm/learnroute/design/PresentOrElseHandler.java
@@ -0,0 +1,19 @@
+package com.madm.learnroute.design;
+
+import java.util.function.Consumer;
+
+@FunctionalInterface
+public interface PresentOrElseHandler<T extends Object> {
+
+    /**
+     * 值不为空时执行消费操作
+     * 值为空时执行其他的操作
+     *
+     * @param action      值不为空时，执行的消费操作
+     * @param emptyAction 值为空时，执行的操作
+     * @return void
+     **/
+    void presentOrElseHandle(Consumer<? super T> action, Runnable emptyAction);
+
+
+}
