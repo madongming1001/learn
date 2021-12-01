ac76dcaa16ce8439f143b427cd64bc314697ecbb
madongming1001@163.com <madongming1001@163.com>
2021-12-01 23:03:23 +0800

    检测心跳代码提交

---
+++ learn-api/src/main/java/com/madm/learnroute/netty/HeartBeatServerHandler.java
@@ -0,0 +1,53 @@
+package com.madm.learnroute.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleStateEvent;
+
+public class HeartBeatServerHandler extends SimpleChannelInboundHandler {
+
+    int readIdleTimes = 0;
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(" ====== > [server] message received : " + msg);
+        if ("Heartbeat Packet".equals(msg)) {
+            ctx.channel().writeAndFlush("ok");
+        } else {
+            System.out.println(" 其他信息处理 ... ");
+        }
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        IdleStateEvent event = (IdleStateEvent) evt;
+
+        String eventType = null;
+        switch (event.state()) {
+            case READER_IDLE:
+                eventType = "读空闲";
+                readIdleTimes++; // 读空闲的计数加1
+                break;
+            case WRITER_IDLE:
+                eventType = "写空闲";
+                // 不处理
+                break;
+            case ALL_IDLE:
+                eventType = "读写空闲";
+                // 不处理
+                break;
+        }
+        System.out.println(ctx.channel().remoteAddress() + "超时事件：" + eventType);
+        if (readIdleTimes > 3) {
+            System.out.println(" [server]读空闲超过3次，关闭连接，释放更多资源");
+            ctx.channel().writeAndFlush("idle close");
+            ctx.channel().close();
+        }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.err.println("=== " + ctx.channel().remoteAddress() + " is active ===");
+    }
+
+
+}
