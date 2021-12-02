7cba122a5e7f3ac99142bc46f53821ae758a5346
madongming1001@163.com <madongming1001@163.com>
2021-12-02 21:13:46 +0800

    netty

--- learn-api/src/main/java/com/madm/learnroute/zookeeper/ConfigCenter.java
+++ learn-api/src/main/java/com/madm/learnroute/zookeeper/ConfigCenter.java
@@ -1,12 +1,10 @@
 package com.madm.learnroute.zookeeper;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.util.concurrent.CountDownLatch;
 
-@Slf4j
 public class ConfigCenter {
     private final static String CONNECT_STR = "192.168.109.200:2181";
     private final static Integer SESSION_TIMEOUT = 30 * 1000;
@@ -16,7 +14,6 @@ public class ConfigCenter {
         zookeeper = new ZooKeeper(CONNECT_STR, SESSION_TIMEOUT, (event) -> {
             if (event.getType() == Watcher.Event.EventType.None && event.getState() == Watcher.Event.KeeperState.SyncConnected) {
                 countDownLatch.countDown();
-                log.info("连接已建立");
             }
         });
         countDownLatch.await();
