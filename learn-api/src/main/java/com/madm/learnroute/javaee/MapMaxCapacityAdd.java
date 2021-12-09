def4f6fd15b3a78306e5a880023baed762600a0d
madongming1001@163.com <madongming1001@163.com>
2021-12-09 15:58:15 +0800

    内存布局

--- learn-api/src/main/java/com/madm/learnroute/javaee/MapMaxCapacityAdd.java
+++ learn-api/src/main/java/com/madm/learnroute/javaee/MapMaxCapacityAdd.java
@@ -1,5 +1,16 @@
 package com.madm.learnroute.javaee;
 
+import com.madm.learnroute.netty.codec.User;
+import org.apache.lucene.util.RamUsageEstimator;
+import org.aspectj.weaver.ast.Var;
+
 public class MapMaxCapacityAdd {
-    Object obj = new Object();
+    private volatile Object myobj = new Object();
+    private User user = new User();
+
+    public static void main(String[] args) {
+        User user = new User();
+        long x = RamUsageEstimator.sizeOfObject(user);
+        System.out.println(x);
+    }
 }
