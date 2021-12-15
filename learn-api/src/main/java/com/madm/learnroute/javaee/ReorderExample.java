a589ed908aaf73cd9fa0c851d678e02ef0691441
madongming1001@163.com <madongming1001@163.com>
2021-12-15 15:55:49 +0800

    提交注释C

--- learn-api/src/main/java/com/madm/learnroute/javaee/ReorderExample.java
+++ learn-api/src/main/java/com/madm/learnroute/javaee/ReorderExample.java
@@ -3,13 +3,14 @@ package com.madm.learnroute.javaee;
 public class ReorderExample {
     int a = 0;
     boolean flag = false;
+
     public void writer(){
-       a = 1;
-       flag = true;
+       a = 1; //1
+       flag = true;//2
     }
     public void reader(){
-        if(flag){
-            int i = a * a;
+        if(flag){//3
+            int i = a * a;//4
             System.out.println(i);
         }
     }
