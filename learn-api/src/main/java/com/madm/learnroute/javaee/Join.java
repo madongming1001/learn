25c6323b9427186bab494e0057861c8448b21011
madongming1001@163.com <madongming1001@163.com>
2021-12-18 14:20:56 +0800

    leetcode 24

---
+++ learn-api/src/main/java/com/madm/learnroute/javaee/Join.java
@@ -0,0 +1,32 @@
+package com.madm.learnroute.javaee;
+
+import java.util.concurrent.TimeUnit;
+
+public class Join {
+    public static void main(String[] args) throws InterruptedException {
+        Thread previous = Thread.currentThread();
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new Domino(previous), String.valueOf(i));
+            thread.start();
+            previous = thread;
+        }
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(Thread.currentThread().getName() + " terminate.");
+    }
+    static class Domino implements Runnable{
+        private Thread thread;
+        public Domino(Thread thread){
+            this.thread = thread;
+        }
+
+        @Override
+        public void run() {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " terminate.");
+        }
+    }
+}
