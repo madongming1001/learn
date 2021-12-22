15b1487e259bd89382f268cce41635d5e271726f
madongming <madongming1001@163.com>
2021-12-22 11:06:31 +0800

    更新

---
+++ learn-api/src/main/java/com/madm/learnroute/javaee/Cache.java
@@ -0,0 +1,44 @@
+package com.madm.learnroute.javaee;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Cache {
+    static Map<String, Object> map = new HashMap<String, Object>();
+    static ReentrantReadWriteLock rw = new ReentrantReadWriteLock();
+    static Lock readLock = rw.readLock();
+    static Lock writeLock = rw.writeLock();
+
+    public static final Object get(String key) {
+        readLock.lock();
+        try {
+            return map.get(key);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public static final Object put(String key, Object value) {
+        writeLock.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public static final void clear() {
+        writeLock.lock();
+        try {
+            map.clear();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(0x0000FFFF));
+    }
+}
