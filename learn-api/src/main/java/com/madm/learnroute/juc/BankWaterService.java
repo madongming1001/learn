b9d33be58344dca9e783a8d0b3c31cc651636a2b
madongming <madongming1001@163.com>
2021-12-23 11:19:51 +0800

    juc

--- learn-api/src/main/java/com/madm/learnroute/juc/BankWaterService.java
+++ learn-api/src/main/java/com/madm/learnroute/juc/BankWaterService.java
@@ -6,11 +6,11 @@ import java.util.concurrent.*;
 public class BankWaterService implements Runnable {
 
     /*** 创建4个屏障，处理完之后执行当前类的run方法 */
-    private CyclicBarrier c = new CyclicBarrier(4, this);
+    private CyclicBarrier cyclic = new CyclicBarrier(4, this);
     /*** 假设只有4个sheet，所以只启动4个线程 */
     private Executor executor = Executors.newFixedThreadPool(4);
     /*** 保存每个sheet计算出的银流结果 */
-    private ConcurrentHashMap<String, Integer> sheetBankWaterCount = new ConcurrentHashMap<String, Integer>();
+    private ConcurrentHashMap<String, Integer> sheetBankWaterCount = new ConcurrentHashMap();
 
     private void count() {
         for (int i = 0; i < 4; i++) {
@@ -19,14 +19,13 @@ public class BankWaterService implements Runnable {
                 public void run() { // 计算当前sheet的银流数据，计算代码省略
                     sheetBankWaterCount.put(Thread.currentThread().getName(), 1); // 银流计算完成，插入一个屏障
                     try {
-                        c.await();
+                        cyclic.await();
                     } catch (InterruptedException | BrokenBarrierException e) {
                         e.printStackTrace();
                     }
                 }
             });
         }
-
     }
 
     @Override
---
+++ learn-api/src/main/java/com/madm/learnroute/juc/SemaphoreTest.java
@@ -0,0 +1,30 @@
+package com.madm.learnroute.juc;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+    private static final int THREAD_COUNT = 30;
+
+    private static Semaphore s = new Semaphore(10);
+    private static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            threadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        s.acquire();
+                        System.out.println("save data");
+                        s.release();
+                    } catch (InterruptedException e) {
+                    }
+                }
+            });
+        }
+        threadPool.shutdown();
+    }
+}
