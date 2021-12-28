9d69db1a0a4895f76bad678717b823130755917d
madongming <madongming1001@163.com>
2021-12-28 11:06:34 +0800

    binarytree ergodic

--- learn-ds-algorithm/src/main/java/madm/data_structure/BinaryTree.java
+++ learn-ds-algorithm/src/main/java/madm/data_structure/BinaryTree.java
@@ -5,15 +5,15 @@ import lombok.Data;
 
 import java.util.LinkedList;
 import java.util.Objects;
+import java.util.Queue;
+import java.util.Stack;
 
-public class TwoBinaryTree {
+public class BinaryTree {
 
-    @Data
     static class TreeNode<T> {
         T data;
-        static TreeNode leftChild;
-        static TreeNode rightChild;
-
+        public TreeNode leftChild;
+        public TreeNode rightChild;
         public TreeNode(T data) {
             this.data = data;
         }
@@ -60,6 +60,28 @@ public class TwoBinaryTree {
         preOrderTraveral(node.leftChild);
         preOrderTraveral(node.rightChild);
         System.out.println(node.data);
+    }
+
+    /**
+     * 二叉树非递归前序遍历
+     */
+    public static void preOrderTraveralWithStack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            //迭代访问节点的左孩子，并入栈
+            while (treeNode != null) {
+                System.out.println(treeNode.data);
+                stack.push(treeNode);
+                treeNode = treeNode.leftChild;
+            }
+            //如果节点没有左孩子，则弹出栈顶节点，访问节点右孩子
+            if (!stack.isEmpty()) {
+                treeNode = stack.pop();
+                treeNode = treeNode.rightChild;
+            }
+
+        }
     }
 
 
