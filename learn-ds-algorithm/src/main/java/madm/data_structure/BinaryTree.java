973dfcbb8577c4e6d602e1e5064dde36424187a0
madongming <madongming1001@163.com>
2021-12-29 10:28:50 +0800

    41

--- learn-ds-algorithm/src/main/java/madm/data_structure/BinaryTree.java
+++ learn-ds-algorithm/src/main/java/madm/data_structure/BinaryTree.java
@@ -119,7 +119,6 @@ public class BinaryTree {
             head = new Node(data);
             return;
         }
-
         Node<Integer> p = head;
         while (p != null) {
             if (data > p.value) {
@@ -136,6 +135,41 @@ public class BinaryTree {
                 p = p.left;
             }
         }
+    }
+
+
+    public void delete(int data) {
+        Node<Integer> p = head; // p指向要删除的节点，初始化指向根节点
+        Node<Integer> pp = null; // pp记录的是p的父节点
+        while (p != null && p.value != data) {
+            pp = p;
+            if (data > p.value) p = p.right;
+            else p = p.left;
+        }
+        if (p == null) return; // 没有找到
+
+        // 要删除的节点有两个子节点
+        if (p.left != null && p.right != null) { // 查找右子树中最小节点
+            Node<Integer> minP = p.right;
+            Node<Integer> minPP = p; // minPP表示minP的父节点
+            while (minP.left != null) {
+                minPP = minP;
+                minP = minP.left;
+            }
+            p.value = minP.value; // 将minP的数据替换到p中
+            p = minP; // 下面就变成了删除minP了
+            pp = minPP;
+        }
+
+        // 删除节点是叶子节点或者仅有一个子节点
+        Node child; // p的子节点
+        if (p.left != null) child = p.left;
+        else if (p.right != null) child = p.right;
+        else child = null;
+
+        if (pp == null) head = child; // 删除的是根节点
+        else if (pp.left == p) pp.left = child;
+        else pp.right = child;
     }
 
 }
