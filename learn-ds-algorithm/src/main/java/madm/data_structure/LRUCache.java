6daef22c315ef7dd67b0c229558cddead6bcbf94
madongming <madongming1001@163.com>
2021-12-29 21:42:13 +0800

    LRU

--- learn-ds-algorithm/src/main/java/madm/data_structure/LRUCache.java
+++ learn-ds-algorithm/src/main/java/madm/data_structure/LRUCache.java
@@ -37,5 +37,49 @@ public class LRUCache<K, V> {
     }
 
     public void moveNodeToTail(Node node) {
+        if (tail == node) {
+            return;
+        }
+        if (head == node) {
+            this.head = node.next;
+            this.head.pre = null;
+        } else {
+            //把节点从链中拿掉
+            node.pre.next = node.next;
+            node.next.pre = node.pre;
+        }
+        node.pre = tail;
+        node.next = null;
+        tail.next = node;
+        tail = node;
+    }
+
+    public void removeHeadNode() {
+        if (head == null) {
+            return;
+        }
+        Node res = head;
+        if (head == tail) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = res.next;
+            res.next = null;//Help GC
+            this.head.pre = null;
+        }
+    }
+
+    public void addNode(Node<K, V> newNode) {
+        if (newNode == null) {
+            return;
+        }
+        if (head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.pre = tail;
+            tail = newNode;
+        }
     }
 }
