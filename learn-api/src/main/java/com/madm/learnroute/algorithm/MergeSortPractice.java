ab3f49803a8ca912af557d3ae5437d0ffad3cff9
madongming <madongming1001@163.com>
2021-12-30 15:35:38 +0800

    print odd number and even number

--- learn-api/src/main/java/com/madm/learnroute/algorithm/MergeSortPractice.java
+++ learn-api/src/main/java/com/madm/learnroute/algorithm/MergeSortPractice.java
@@ -5,44 +5,38 @@ import com.madm.learnroute.util.PrintUtils;
 public class MergeSortPractice {
     public static void main(String[] args) {
 //        int[] arr = {5,8,6,3,9,2,1,7};
-        int[] arr = {7, 6, 5, 3, 1, 4, 5, 6, 7};
-        sort(arr, 0, arr.length - 1);
+        int[] arr = {5, 2, 3, 1, 4};
+        group(arr, 0, arr.length - 1);
         PrintUtils.printArray(arr);
     }
 
-    public static void sort(int[] array, int start, int end) {
+    public static int[] group(int[] arr, int start, int end) {
         if (start < end) {
             int mid = start + ((end - start) >> 1);
-            sort(array, start, mid);
-            sort(array, mid + 1, end);
-            mergeSort(array, start, mid, end);
+            group(arr, start, mid);
+            group(arr, mid + 1, end);
+            merge(arr, start, mid, end);
         }
+        return arr;
     }
 
-    /**
-     * 在排序好的基础上进行合并
-     *
-     * @param array
-     * @param start
-     * @param mid
-     * @param end
-     */
-    private static void mergeSort(int[] array, int start, int mid, int end) {
-        int[] temp = new int[end - start + 1];
+    public static void merge(int[] array, int start, int mid, int end) {
+        int[] cond = new int[end - start + 1];
         int p1 = start;
         int p2 = mid + 1;
+        //他是数组的地址
         int p = 0;
         while (p1 <= mid && p2 <= end) {
             if (array[p1] <= array[p2]) {
-                temp[p++] = array[p1++];
+                cond[p++] = array[p1++];
             } else {
-                temp[p++] = array[p2++];
+                cond[p++] = array[p2++];
             }
         }
-        while (p1 <= mid) temp[p++] = array[p1++];
-        while (p2 <= end) temp[p++] = array[p2++];
-        for (int i = 0; i < temp.length; i++) {
-            array[i + start] = temp[i];
+        while (p1 <= mid) cond[p++] = array[p1++];
+        while (p2 <= end) cond[p++] = array[p2++];
+        for (int i = 0; i < cond.length; i++) {
+            array[i + start] = cond[i];
         }
     }
 }
---
+++ learn-api/src/main/java/com/madm/learnroute/javaee/ThreadPrintDemo.java
@@ -0,0 +1,47 @@
+package com.madm.learnroute.javaee;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadPrintDemo {
+    static AtomicInteger cxsNum = new AtomicInteger(0);
+    static volatile boolean flag = false;
+
+    public static void main(String[] args) {
+        Thread t1 = printNumber1();
+        Thread t2 = printNumber2();
+        t1.start();
+        t2.start();
+    }
+
+    private static Thread printNumber2() {
+        Thread t2 = new Thread(() -> {
+            for (; 100 > cxsNum.get(); ) {
+                if (flag && (cxsNum.incrementAndGet() % 2 != 0)) {
+                    try {
+                        Thread.sleep(100);// 防止打印速度过快导致混乱
+                    } catch (InterruptedException e) {            //NO
+                    }
+                    System.out.println(cxsNum.get());
+                    flag = false;
+                }
+            }
+        });
+        return t2;
+    }
+
+    private static Thread printNumber1() {
+        Thread t1 = new Thread(() -> {
+            for (; 100 > cxsNum.get(); ) {
+                if (!flag && (cxsNum.get() == 0 || cxsNum.incrementAndGet() % 2 == 0)) {
+                    try {
+                        Thread.sleep(100);// 防止打印速度过快导致混乱
+                    } catch (InterruptedException e) {            //NO
+                    }
+                    System.out.println(cxsNum.get());
+                    flag = true;
+                }
+            }
+        });
+        return t1;
+    }
+}
