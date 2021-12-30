73498e081fc5066b080385c0293cff950abfbdd0
madongming <madongming1001@163.com>
2021-12-30 21:40:28 +0800

    lokc

---
+++ learn-api/src/main/java/com/madm/learnroute/javaee/LinkedBlockingQueuePractice.java
@@ -0,0 +1,10 @@
+package com.madm.learnroute.javaee;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class LinkedBlockingQueuePractice {
+    public static void main(String[] args) {
+        LinkedBlockingQueue<Object> objects1 = new LinkedBlockingQueue<>();
+        LinkedBlockingQueue<Object> objects2 = new LinkedBlockingQueue<>(10);
+    }
+}
