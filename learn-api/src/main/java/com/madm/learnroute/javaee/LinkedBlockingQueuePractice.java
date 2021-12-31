46f85f0d53ba69fc3693b70bb99d17d4aa797ea5
madongming <madongming1001@163.com>
2021-12-31 15:54:33 +0800

    heap

--- learn-api/src/main/java/com/madm/learnroute/javaee/LinkedBlockingQueuePractice.java
+++ learn-api/src/main/java/com/madm/learnroute/javaee/LinkedBlockingQueuePractice.java
@@ -6,5 +6,18 @@ public class LinkedBlockingQueuePractice {
     public static void main(String[] args) {
         LinkedBlockingQueue<Object> objects1 = new LinkedBlockingQueue<>();
         LinkedBlockingQueue<Object> objects2 = new LinkedBlockingQueue<>(10);
+        displacement(4,3);
+    }
+
+    /**
+     * 不用额外变量交换两个整数的值
+     */
+    public static void displacement(int a, int b) {
+        a = a ^ b;
+        System.out.println("第一步a是：" + a);
+        b = a ^ b;
+        System.out.println("第二步b是：" + b);
+        a = a ^ b;
+        System.out.println("第三步a是：" + a);
     }
 }
