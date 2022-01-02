2897f225beb8305f77e9558d164891777f72da4c
madongming <madongming1001@163.com>
2022-01-02 15:05:59 +0800

    格式调整

--- learn-api/src/main/java/com/madm/learnroute/edu/jmm/Jmm04_CodeAtomic.java
+++ learn-api/src/main/java/com/madm/learnroute/edu/jmm/Jmm04_CodeAtomic.java
@@ -25,9 +25,9 @@ public class Jmm04_CodeAtomic {
     public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++) {
-            Thread thread = new Thread(()->{
+            Thread thread = new Thread(() -> {
                 for (int j = 0; j < 1000; j++) {
-                    synchronized (object){
+                    synchronized (object) {
                         counter++;//分三步- 读，自加，写回
                         //读
                     }
