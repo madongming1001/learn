1af74b592bb90096e437285cd63ecd312f9d3263
madongming <madongming1001@163.com>
2022-01-04 16:49:23 +0800

    knapsack

--- learn-ds-algorithm/src/main/java/madm/data_structure/Graph.java
+++ learn-ds-algorithm/src/main/java/madm/data_structure/Graph.java
@@ -64,7 +64,34 @@ public class Graph { // 无向图
         System.out.print(t + " ");
     }
 
-    public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE).length());
+
+    boolean found = false; // 全局变量或者类成员变量
+
+    public void dfs(int s, int t) {
+        found = false;
+        boolean[] visited = new boolean[v];
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        recurDfs(s, t, visited, prev);
+        print(prev, s, t);
     }
+
+    private void recurDfs(int w, int t, boolean[] visited, int[] prev) {
+        if (found == true) return;
+        visited[w] = true;
+        if (w == t) {
+            found = true;
+            return;
+        }
+        for (int i = 0; i < adj[w].size(); ++i) {
+            int q = adj[w].get(i);
+            if (!visited[q]) {
+                prev[q] = w;
+                recurDfs(q, t, visited, prev);
+            }
+        }
+    }
+
 }
