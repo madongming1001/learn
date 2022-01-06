a3941bdc1a741e12c268004c0da3493dc40bc88b
madongming <madongming1001@163.com>
2022-01-06 14:30:11 +0800

    matrix iterator

---
+++ learn-ds-algorithm/src/main/java/madm/data_structure/BinaryTreeNode.java
@@ -0,0 +1,12 @@
+package madm.data_structure;
+
+class TrieNode {
+    char data;
+    TrieNode[] children;
+}
+
+public class BinaryTreeNode {
+    char data;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+}
--- learn-ds-algorithm/src/main/java/madm/data_structure/DynamicProgramming2.java
+++ learn-ds-algorithm/src/main/java/madm/data_structure/DynamicProgramming2.java
@@ -118,13 +118,49 @@ public class DynamicProgramming2 {
         return minv;
     }
 
+
+    /**
+     * 最长公共子串长度
+     */
+    public int lcs(char[] a, int n, char[] b, int m) {
+        int[][] maxlcs = new int[n][m];
+        for (int j = 0; j < m; ++j) {//初始化第0行：a[0..0]与b[0..j]的maxlcs
+            if (a[0] == b[j]) maxlcs[0][j] = 1;
+            else if (j != 0) maxlcs[0][j] = maxlcs[0][j - 1];
+            else maxlcs[0][j] = 0;
+        }
+        for (int i = 0; i < n; ++i) {//初始化第0列：a[0..i]与b[0..0]的maxlcs
+            if (a[i] == b[0]) maxlcs[i][0] = 1;
+            else if (i != 0) maxlcs[i][0] = maxlcs[i - 1][0];
+            else maxlcs[i][0] = 0;
+        }
+        for (int i = 1; i < n; ++i) { // 填表
+            for (int j = 1; j < m; ++j) {
+                if (a[i] == b[j]) maxlcs[i][j] = max(
+                        maxlcs[i - 1][j], maxlcs[i][j - 1], maxlcs[i - 1][j - 1] + 1);
+                else maxlcs[i][j] = max(
+                        maxlcs[i - 1][j], maxlcs[i][j - 1], maxlcs[i - 1][j - 1]);
+            }
+        }
+        return maxlcs[n - 1][m - 1];
+    }
+
+    private int max(int x, int y, int z) {
+        int maxv = Integer.MIN_VALUE;
+        if (x > maxv) maxv = x;
+        if (y > maxv) maxv = y;
+        if (z > maxv) maxv = z;
+        return maxv;
+    }
+
     public static void main(String[] args) {
-//        int[][] matrix =
-//                {{1, 3, 5, 9}, {2, 1, 3, 4}, {5, 2, 6, 7}, {6, 8, 4, 3}};
-        DynamicProgramming2 dp = new DynamicProgramming2();
-//        System.out.println(dp.minDistDP(matrix, 4));
-        char[] a = {'m', 'i', 't', 'c', 'm', 'u'};
-        char[] b = {'c', 't', 'a', 'c', 'n', 'u'};
-        System.out.println(dp.lwstDP(a, a.length, b, b.length));
+        int[][] matrix =
+                {{1, 3, 5, 9}, {2, 1, 3, 4}, {5, 2, 6, 7}, {6, 8, 4, 3}};
+        System.out.println(matrix.length);
+//        DynamicProgramming2 dp = new DynamicProgramming2();
+////        System.out.println(dp.minDistDP(matrix, 4));
+//        char[] a = {'m', 'i', 't', 'c', 'm', 'u'};
+//        char[] b = {'c', 't', 'a', 'c', 'n', 'u'};
+//        System.out.println(dp.lwstDP(a, a.length, b, b.length));
     }
 }
