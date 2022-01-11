794f5e53bb8478c721ac5a146bc697338c592008
madongming <madongming1001@163.com>
2022-01-11 19:47:24 +0800

    threadlocal

--- learn-api/src/main/java/com/madm/learnroute/javaee/ThreadLocalPractice.java
+++ learn-api/src/main/java/com/madm/learnroute/javaee/ThreadLocalPractice.java
@@ -1,9 +1,26 @@
 package com.madm.learnroute.javaee;
 
+class ThreadLocalTask extends Thread {
+    ThreadLocal<Integer> threadLocal;
+    Integer threadId;
+
+    {
+        threadLocal = new ThreadLocal<>();
+    }
+    public ThreadLocalTask(int threadId) {
+        this.threadId = threadId;
+    }
+
+    @Override
+    public void run() {
+        threadLocal.set(threadId);
+        System.out.println("threadLocal值为：" + threadLocal.get() + "，线程id为：" + threadId);
+    }
+}
 
 public class ThreadLocalPractice {
-    private static ThreadLocal threadLocal = new ThreadLocal<>();
     public static void main(String[] args) {
-        threadLocal.set("threadLocal对象赋值");
+        new Thread(new ThreadLocalTask(1)).start();
+        new Thread(new ThreadLocalTask(2)).start();
     }
 }
