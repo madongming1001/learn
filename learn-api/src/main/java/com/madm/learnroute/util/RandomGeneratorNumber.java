33238ff062f4e360465dc309cd18b3598da67d57
madongming <madongming1001@163.com>
2022-01-15 11:21:22 +0800

    整合mybatis plus

--- learn-api/src/main/java/com/madm/learnroute/util/RandomGeneratorNumber.java
+++ learn-api/src/main/java/com/madm/learnroute/util/RandomGeneratorNumber.java
@@ -5,10 +5,10 @@ public class RandomGeneratorNumber {
 
     public static int[] createArrayOfSize(int size) {
         int[] result = new int[size];
-        int max  = 0;
+        int max = 0;
         for (int i = 0; i < size; i++) {
-            result[i] = (int)(Math.random()*size) + 1;
-            if(max < result[i]){
+            result[i] = (int) (Math.random() * size) + 1;
+            if (max < result[i]) {
                 max = result[i];
             }
         }
