afc03dfe7120f76703b5fd17daa2bd8d93b3568a
madongming <madongming1001@163.com>
2022-01-17 11:11:59 +0800

    多余包删除

--- learn-api/src/main/java/com/madm/learnroute/mybatis/MybatisPractice.java
+++ learn-api/src/main/java/com/madm/learnroute/mybatis/MybatisPractice.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 public class MybatisPractice {
