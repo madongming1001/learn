24090d5a9f4b14cabea1dccf8b6cb88c2e59d276
madongming <madongming1001@163.com>
2022-01-18 18:39:43 +0800

    SimpleDateFormat prove not safe

--- learn-api/src/main/java/com/madm/learnroute/funcationinterface/PredicateTest.java
+++ learn-api/src/main/java/com/madm/learnroute/funcationinterface/PredicateTest.java
@@ -2,12 +2,15 @@ package com.madm.learnroute.funcationinterface;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateTest {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("北京", "南京", "东京", "长安", "洛阳");
+        List<String> strings = Collections.synchronizedList(list);
+        strings.add("!");
         list = filterStr(list, s -> s.contains("京"));
         list.forEach(System.out::println);
     }
