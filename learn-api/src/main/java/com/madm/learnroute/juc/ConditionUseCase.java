24c9db4a0c84297da99f50ce30a47ad9fa46173a
madongming <madongming1001@163.com>
2022-02-24 20:32:29 +0800

    学习elasticsearch day 2

--- learn-api/src/main/java/com/madm/learnroute/juc/ConditionUseCase.java
+++ learn-api/src/main/java/com/madm/learnroute/juc/ConditionUseCase.java
@@ -4,6 +4,9 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ *
+ */
 public class ConditionUseCase {
     Lock lock = new ReentrantLock();
     Condition condition = lock.newCondition();
