202b51347a459b934ccdd6371733c7aa7298db04
madongming <madongming1001@163.com>
2022-03-05 23:00:00 +0800

    importselector

--- learn-api/src/main/java/com/madm/learnroute/spring/ImportSelectorPractice.java
+++ learn-api/src/main/java/com/madm/learnroute/spring/ImportSelectorPractice.java
@@ -3,11 +3,18 @@ package com.madm.learnroute.spring;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.function.Predicate;
+
 public class ImportSelectorPractice implements ImportSelector {
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
         return new String[]{"com.madm.learnroute.spring.BusinessEvent.java"};
+    }
+
+    @Override
+    public Predicate<String> getExclusionFilter() {
+        return ImportSelector.super.getExclusionFilter();
     }
 
 }
