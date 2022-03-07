441ec1151c8322534e43177960675835a8ccc710
madongming <madongming1001@163.com>
2022-03-07 19:55:52 +0800

    spi添加

--- learn-api/src/main/java/com/madm/learnroute/spring/MyImportSelector.java
+++ learn-api/src/main/java/com/madm/learnroute/spring/MyImportSelector.java
@@ -5,10 +5,11 @@ import org.springframework.core.type.AnnotationMetadata;
 
 import java.util.function.Predicate;
 
-public class ImportSelectorPractice implements ImportSelector {
+public class MyImportSelector implements ImportSelector {
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        //字符串必须是类的完整限定名 getbean 不能根据名字获取 只能根据类型获取
         return new String[]{"com.madm.learnroute.spring.BusinessEvent.java"};
     }
 
