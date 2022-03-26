21b1727b2fec9a6ab37300b29babedaf27b4d6d9
madongming <madongming1001@163.com>
2022-03-26 21:04:00 +0800

    applicationListener

---
+++ learn-api/src/main/java/com/madm/learnroute/spring/MyApplicationListener.java
@@ -0,0 +1,11 @@
+package com.madm.learnroute.spring;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class MyApplicationListener implements ApplicationListener {
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+
+    }
+}
