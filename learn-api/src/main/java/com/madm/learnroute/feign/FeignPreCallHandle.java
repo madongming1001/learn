c748e1c9f62549b4eee4014044e9dd949141243d
madongming <madongming1001@163.com>
2022-04-16 22:37:19 +0800

    .

--- learn-api/src/main/java/com/madm/learnroute/feign/FeignPreCallHandle.java
+++ learn-api/src/main/java/com/madm/learnroute/feign/FeignPreCallHandle.java
@@ -6,7 +6,7 @@ import feign.RequestTemplate;
 /**
  * feign调用之前回掉
  */
-public class FeignPreCallProcessing implements RequestInterceptor {
+public class FeignPreCallHandle implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
 
