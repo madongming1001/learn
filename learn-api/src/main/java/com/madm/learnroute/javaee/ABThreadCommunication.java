558351756d7972ad272631353651a6c9d76a6600
madongming <madongming1001@163.com>
2022-04-20 15:37:18 +0800

    线程通信输出顺序修改

--- learn-api/src/main/java/com/madm/learnroute/javaee/ABThreadCommunication.java
+++ learn-api/src/main/java/com/madm/learnroute/javaee/ABThreadCommunication.java
@@ -6,25 +6,19 @@ public class ABThreadCommunication {
     private volatile static boolean b = false;
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("发送a线程的信号");
-                a = true;
-                while (!b) {
-                }
-                System.out.println("收到b线程的信号");
+        Thread t1 = new Thread(() -> {
+            System.out.println("发送a线程的信号");
+            a = true;
+            while (!b) {
             }
+            System.out.println("收到b线程的信号");
         });
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!a) {
-                }
-                System.out.println("收到a线程的信号");
-                System.out.println("发送b线程的信号");
-                b = true;
+        Thread t2 = new Thread(() -> {
+            System.out.println("收到a线程的信号");
+            while (!a) {
             }
+            System.out.println("发送b线程的信号");
+            b = true;
         });
         t1.start();
         t2.start();
