188c9b562d9d19a86501eb65cf3362412c53a80c
madongming <madongming1001@163.com>
2022-04-21 17:22:24 +0800

    压缩工具类zstd snappy

--- learn-api/src/main/java/com/madm/learnroute/javaee/LocalDateTimePractice.java
+++ learn-api/src/main/java/com/madm/learnroute/javaee/LocalDateTimePractice.java
@@ -1,15 +1,9 @@
 package com.madm.learnroute.javaee;
 
 import lombok.SneakyThrows;
-import org.joda.time.format.DateTimeFormatter;
-import org.springframework.util.Assert;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneOffset;
 import java.util.Date;
 
 public class LocalDateTimePractice {
@@ -24,11 +18,7 @@ public class LocalDateTimePractice {
         //创建多个线程2
         for (int i = 0; i < 10; i++) {
             new Thread(() -> {
-                try {
-                    System.out.println(sdf.parse("2020-9-9 11:11:11"));
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
+                System.out.println(sdf.format(new Date()));
                 //线程启动 3
             }).start();
         }
