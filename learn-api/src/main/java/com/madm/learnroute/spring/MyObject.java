aabee0e5197d1b7fee77f544604e8621a85460d3
madongming <madongming1001@163.com>
2022-04-22 15:23:55 +0800

    .

--- learn-api/src/main/java/com/madm/learnroute/spring/MyObject.java
+++ learn-api/src/main/java/com/madm/learnroute/spring/MyObject.java
@@ -5,17 +5,16 @@ import com.google.common.base.Supplier;
 import io.lettuce.core.output.StatusOutput;
 
 public class MyObject {
-    public static void getObject(ObjectFactory<?> objectFactory){
+
+    public static void getObject(ObjectFactory<?> objectFactory) {
         System.out.println(objectFactory.getObject());
     }
 
     public static void main(String[] args) {
         String name = "Lisa";
-//        getObject(() -> getObjectByName(name));
-//
-//        Supplier<String> getObject = () -> {
-//            return new String("123455");
-//        };
+        getObject(() -> getObjectByName(name));
+
+        Supplier<String> getObject = () -> "123455";
         int i = "33333999".hashCode();
         int j = "44444999".hashCode();
         int disturbingTermI = i ^ (i >>> 16);
@@ -37,7 +36,7 @@ public class MyObject {
     }
 
     private static Object getObjectByName(String name) {
-        if(name.startsWith("L")){
+        if (name.startsWith("L")) {
             return "自定义方法测试返回";
         }
         return name;
