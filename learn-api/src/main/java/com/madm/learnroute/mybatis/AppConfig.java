2d8d83cc47b65f8507c71ebf3d0428b67642c33e
madongming <madongming1001@163.com>
2022-05-03 16:21:24 +0800

    休息一天

--- learn-api/src/main/java/com/madm/learnroute/mybatis/AppConfig.java
+++ learn-api/src/main/java/com/madm/learnroute/mybatis/AppConfig.java
@@ -5,10 +5,17 @@ import com.madm.learnroute.mybatis.mybatis.spring.ZhoyuMapperScan;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -16,44 +23,12 @@ import java.io.InputStream;
 @ZhoyuMapperScan("com.zhouyu.mapper")
 public class AppConfig {
 
-	@Bean
-	public SqlSessionFactory sqlSessionFactory() throws IOException {
-		InputStream inputStream = Resources.getResourceAsStream("mybatis.xml");
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		return sqlSessionFactory;
-	}
-
-//	@Bean
-//	public JdbcTemplate jdbcTemplate() {
-//		return new JdbcTemplate(dataSource());
-//	}
-//
-//	@Bean
-//	public PlatformTransactionManager transactionManager() {
-//		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
-//		transactionManager.setDataSource(dataSource());
-//		return transactionManager;
-//	}
-//
-
-
-//	@Bean
-//	public DataSource dataSource() {
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setUrl("jdbc:mysql://127.0.0.1:4000/tuling?characterEncoding=utf-8&amp;useSSL=false");
-//		dataSource.setUsername("root");
-//		dataSource.setPassword("Zhouyu123456***");
-//		return dataSource;
-//	}
-
-
-//	@Bean
-//	public MapperScannerConfigurer configurer(){
-//		MapperScannerConfigurer configurer = new MapperScannerConfigurer();
-//		configurer.setBasePackage("com.zhouyu.mapper");
-//
-//		return configurer;
-//	}
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        return sqlSessionFactory;
+    }
 
 //	@Bean
 //	public SqlSessionFactory sqlSessionFactory() throws Exception {
@@ -62,7 +37,35 @@ public class AppConfig {
 //		return sessionFactoryBean.getObject();
 //	}
 
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
+    }
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:4000/tuling?characterEncoding=utf-8&amp;useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Zhouyu123456***");
+        return dataSource;
+    }
+
+
+    @Bean
+    public MapperScannerConfigurer configurer() {
+        MapperScannerConfigurer configurer = new MapperScannerConfigurer();
+        configurer.setBasePackage("com.zhouyu.mapper");
+        return configurer;
+    }
 
 
 }
