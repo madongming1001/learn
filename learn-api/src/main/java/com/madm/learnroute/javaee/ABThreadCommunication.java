0552854bbacf32adbd8bdf0c16c500ea4372ac4e
madongming <madongming1001@163.com>
2022-05-05 19:31:04 +0800

    防护cpu占用

--- learn-api/src/main/java/com/madm/learnroute/javaee/ABThreadCommunication.java
+++ learn-api/src/main/java/com/madm/learnroute/javaee/ABThreadCommunication.java
@@ -1,5 +1,7 @@
 package com.madm.learnroute.javaee;
 
+import java.util.concurrent.TimeUnit;
+
 public class ABThreadCommunication {
 
     private volatile static boolean a = false;
@@ -10,12 +12,22 @@ public class ABThreadCommunication {
             System.out.println("发送a线程的信号");
             a = true;
             while (!b) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
             System.out.println("收到b线程的信号");
         });
         Thread t2 = new Thread(() -> {
             System.out.println("收到a线程的信号");
             while (!a) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
             System.out.println("发送b线程的信号");
             b = true;
