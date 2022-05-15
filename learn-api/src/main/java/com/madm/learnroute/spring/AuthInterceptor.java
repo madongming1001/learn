e672a0ce967d281b7dee780af946f5dca42bfebb
madongming <madongming1001@163.com>
2022-05-15 16:21:24 +0800

    补提交

--- learn-api/src/main/java/com/madm/learnroute/spring/AuthInterceptor.java
+++ learn-api/src/main/java/com/madm/learnroute/spring/AuthInterceptor.java
@@ -6,14 +6,18 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import java.util.Objects;
 
+/**
+ * 验证方法是否添加@UserAuthenticate注解
+ */
 public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HandlerMethod handlerMethod = (HandlerMethod) handler;
-        UserAuthenticate annotation = handlerMethod.getMethod().getAnnotation(UserAuthenticate.class);
-        if (Objects.isNull(annotation)) {
+        Method method = handlerMethod.getMethod();
+        if (method.getName().equals("restSuccess") && Objects.isNull(method.getAnnotation(UserAuthenticate.class))) {
             return false;
         }
         return true;
