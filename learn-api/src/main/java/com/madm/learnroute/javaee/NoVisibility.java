6735af5a97be47d8b4f21fddf39ac7cd8d85a727
madongming <madongming1001@163.com>
2022-05-20 22:01:42 +0800

    写vue

--- learn-api/src/main/java/com/madm/learnroute/javaee/NoVisibility.java
+++ learn-api/src/main/java/com/madm/learnroute/javaee/NoVisibility.java
@@ -1,7 +1,7 @@
 package com.madm.learnroute.javaee;
 
 public class NoVisibility {
-    private static boolean ready;
+    private static boolean ready = false;
     private static int number;
 
     private static class ReaderThread extends Thread {
@@ -15,8 +15,9 @@ public class NoVisibility {
     }
 
     public static void main(String[] args) {
-//        new ReaderThread().start();
-//        number = 42;
-//        ready = true;
+        ReaderThread thread = new ReaderThread();
+        thread.start();
+        number = 42;
+        ready = true;
     }
 }
