322b023c0c57393c9330cdc1e10bf5e0960437bb
madongming <madongming1001@163.com>
2022-05-28 16:12:09 +0800

    服务发现

---
+++ discovery/src/main/java/com/mdm/controller/ServiceDiscoveryController.java
@@ -0,0 +1,13 @@
+package com.mdm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dongming.ma
+ * @date 2022/5/28 16:01
+ */
+@RestController
+@RequestMapping("service")
+public class ServiceDiscoveryController {
+}
