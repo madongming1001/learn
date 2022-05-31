5e29adb52b5f6169dddf9f345b6aea7ec4614a96
madongming <madongming1001@163.com>
2022-05-31 12:01:51 +0800

    时间转换

--- api/src/main/java/com/madm/learnroute/juc/BankWaterService.java
+++ api/src/main/java/com/madm/learnroute/juc/BankWaterService.java
@@ -44,8 +44,10 @@ public class BankWaterService implements Runnable {
     }
 
     public static void main(String[] args) {
-        BankWaterService bankWaterCount = new BankWaterService();
-        
-        bankWaterCount.count();
+//        BankWaterService bankWaterCount = new BankWaterService();
+//
+//        bankWaterCount.count();
+        long two_day = TimeUnit.DAYS.toMillis(2);
+        System.out.println(two_day);
     }
 }
