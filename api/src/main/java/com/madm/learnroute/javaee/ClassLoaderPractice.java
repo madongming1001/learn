4e112659e5a6de5f7399d2222551cbfb4de1fcae
madongming <madongming1001@163.com>
2022-06-05 12:28:35 +0800

    getResource of class and classloader

--- api/src/main/java/com/madm/learnroute/javaee/ClassLoaderPractice.java
+++ api/src/main/java/com/madm/learnroute/javaee/ClassLoaderPractice.java
@@ -7,6 +7,13 @@ public class ClassLoaderPractice {
 //        System.out.println(ClassLoaderPractice.class.getClassLoader());
 //        System.out.println(7 / 2);
 //        System.out.println(7 >>> 1);
-        System.out.println(ClassLoaderPractice.class.getName());
+        //file:/Users/madongming/IdeaProjects/learn/api/target/classes/com/madm/learnroute/javaee/ 当前同级目录下去找
+        System.out.println(ClassLoaderPractice.class.getResource("logback-spring.xml"));
+        //file:/Users/madongming/IdeaProjects/learn/api/target/classes/ 上层目录下去找
+        System.out.println(ClassLoaderPractice.class.getResource("/logback-spring.xml"));
+        //file:/Users/madongming/IdeaProjects/learn/api/target/classes/ 上层目录下去找
+        System.out.println(ClassLoaderPractice.class.getClassLoader().getResource("logback-spring.xml"));
+        //null 同级目录下去找
+        System.out.println(ClassLoaderPractice.class.getClassLoader().getResource("/logback-spring.xml"));
     }
 }
