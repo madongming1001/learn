5a014df01b16914f9c86986b5f80272b0f930fda
madongming <madongming1001@163.com>
2022-06-06 18:37:30 +0800

    pom版本添加

--- rollback.sh
+++ rollback.sh
@@ -1,2 +1,2 @@
-params="-Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dswagger2markup.skip=true -Dasciidoctor.skip=true"
-./mvnw release:rollback -Darguments="${params}"}
+#!/usr/bin/env bash
+./mvnw versions:revert
