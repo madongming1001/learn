0a69a211f7414ae769eded24764e6634a866d5ca
madongming <madongming1001@163.com>
2022-06-06 22:48:47 +0800

    枚举类调用常数返回的是对象本身！！！

---
+++ api/src/main/java/com/madm/learnroute/javaee/CacheEnumPractice.java
@@ -0,0 +1,13 @@
+package com.madm.learnroute.javaee;
+
+import com.madm.learnroute.constant.CacheEnum;
+
+/**
+ * @author dongming.ma
+ * @date 2022/6/6 22:47
+ */
+public class CacheEnumPractice {
+    public static void main(String[] args) {
+        CacheEnum getMenu = CacheEnum.GET_MENU;
+    }
+}
