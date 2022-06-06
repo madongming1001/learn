b5d14b069e31c9d7b77bbd63e000f1ee54e28854
madongming <madongming1001@163.com>
2022-06-06 18:26:14 +0800

    项目添加mvnw

---
+++ spring-feature/src/test/java/com/mdm/feature/springfeature/SpringFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdm.feature.springfeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
