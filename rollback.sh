dbe7faab890e89b1d067de950fa2d6149ca7778b
madongming <madongming1001@163.com>
2022-06-06 18:49:00 +0800

    添加版本回退文件

--- rollback.sh
+++ rollback.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+#还原到之前版本
 ./mvnw versions:revert
+#清除暂存区
+git restore algorithm/pom.xml
+git restore api/pom.xml
+git restore common/pom.xml
+git restore discovery/pom.xml
+git restore pom.xml
