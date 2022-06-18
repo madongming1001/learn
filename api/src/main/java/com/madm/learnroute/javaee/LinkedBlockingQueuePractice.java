5e8b2f5b1610cf620c20245fb73705b06c72a35b
madongming <madongming1001@163.com>
2022-06-18 23:39:04 +0800

    学习linkedblockingqueue

--- api/src/main/java/com/madm/learnroute/javaee/LinkedBlockingQueuePractice.java
+++ api/src/main/java/com/madm/learnroute/javaee/LinkedBlockingQueuePractice.java
@@ -1,12 +1,28 @@
 package com.madm.learnroute.javaee;
 
+import com.madm.learnroute.jvm.juc.threadPool.ThreadExecutionPractice;
+
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class LinkedBlockingQueuePractice {
-    public static void main(String[] args) {
-        LinkedBlockingQueue<Object> objects1 = new LinkedBlockingQueue<>();
+    private static LinkedBlockingQueue<Object> objects1 = new LinkedBlockingQueue<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        new ThreadExecutionPractice("asyncThread", () -> {
+            try {
+                TimeUnit.SECONDS.sleep(2l);
+                objects1.put("ssss");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        System.out.println("take为空，阻塞");
+        Object take = objects1.take();
+        System.out.println("take有值" + "，值为：" + take);
+        objects1.put("object");
         LinkedBlockingQueue<Object> objects2 = new LinkedBlockingQueue<>(10);
-        displacement(4,3);
+        displacement(4, 3);
     }
 
     /**
@@ -14,10 +30,7 @@ public class LinkedBlockingQueuePractice {
      */
     public static void displacement(int a, int b) {
         a = a ^ b;
-        System.out.println("第一步a是：" + a);
         b = a ^ b;
-        System.out.println("第二步b是：" + b);
         a = a ^ b;
-        System.out.println("第三步a是：" + a);
     }
 }
