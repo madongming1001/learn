6e2b0e97fc5ba916b1b5d6b333224130a189baab
madongming <madongming1001@163.com>
2022-06-26 12:33:23 +0800

    测试为什么提交之后github显示没有提交

--- api/src/main/java/com/madm/learnroute/javaee/LocalDateTimePractice.java
+++ api/src/main/java/com/madm/learnroute/javaee/LocalDateTimePractice.java
@@ -1,9 +1,12 @@
 package com.madm.learnroute.javaee;
 
 import lombok.SneakyThrows;
+import org.springframework.util.Assert;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -17,16 +20,16 @@ public class LocalDateTimePractice {
 
     @SneakyThrows
     public static void main(String[] args) {
-//        Assert.isTrue(LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8")) == System.currentTimeMillis(),
-//                "比较的数是不相等的");
-//        System.out.println(System.currentTimeMillis());
-        //创建多个线程2
-//        for (int i = 0; i < 10; i++) {
-//            new Thread(() -> {
-//                System.out.println(sdf.format(new Date()));
-//                //线程启动 3
-//            }).start();
-//        }
+        Assert.isTrue(LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8")) == System.currentTimeMillis(),
+                "比较的数是不相等的");
+        System.out.println(System.currentTimeMillis());
+//        创建多个线程2
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                System.out.println(sdf.format(new Date()));
+                //线程启动 3
+            }).start();
+        }
         HashMap<String, Long> timeUnitTable = new HashMap();
         timeUnitTable.put("s", 1000L);
         timeUnitTable.put("m", 1000L * 60);
