2021bbc172da93d1890c8c5cc092fe1292f1ca4e
madongming <madongming1001@163.com>
2022-06-29 09:16:16 +0800

    定时任务改为多线程并行

---
+++ api/src/main/java/com/madm/learnroute/config/ScheduleThreadPoolConfig.java
@@ -0,0 +1,22 @@
+package com.madm.learnroute.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+/**
+ * 多线程执行定时任务
+ *
+ * @author dongming.ma
+ * @date 2022/6/29 09:11
+ */
+@Configuration
+public class ScheduleThreadPoolConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        //设定一个长度10的定时任务线程池
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(10));
+    }
+}
