72d43af2198f2a12f55d173e43d0360049d592cf
madongming <madongming1001@163.com>
2022-07-03 13:11:54 +0800

    添加数据源枚举

---
+++ springfeature/src/main/java/com/mdm/springfeature/enums/DataSourceType.java
@@ -0,0 +1,10 @@
+package com.mdm.springfeature.enums;
+
+/**
+ * @author dongming.ma
+ * @date 2022/7/3 13:04
+ */
+public interface DataSourceType {
+    String BADGUY = "BADGUY";
+    String GOODBUY = "GOODBUY";
+}
