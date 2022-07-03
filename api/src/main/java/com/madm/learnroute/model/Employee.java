db68b424679a171420b3f81b825d36938fe8a8af
madongming <madongming1001@163.com>
2022-07-04 00:38:14 +0800

    composite1.0

---
+++ api/src/main/java/com/madm/learnroute/model/Employee.java
@@ -0,0 +1,65 @@
+package com.madm.learnroute.model;
+
+/**
+ * @author dongming.ma
+ * @date 2022/7/3 22:59
+ */
+public class Employee {
+    String name;
+    int age;
+    double salary;
+    long mobile;
+
+    // constructors, getters & setters
+
+
+    public Employee(String name, int age, double salary, long mobile) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employee{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", age=").append(age);
+        sb.append(", salary=").append(salary);
+        sb.append(", mobile=").append(mobile);
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- discovery/src/main/java/com/mdm/DiscoveryApplication.java
+++ discovery/src/main/java/com/mdm/DiscoveryApplication.java
@@ -2,9 +2,10 @@ package com.mdm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableDiscoveryClient
 public class DiscoveryApplication {
     public static void main(String[] args) {
