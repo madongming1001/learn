ef193bae3abf5f492f2fe280ce86c76b4ff9f4ff
madongming <madongming1001@163.com>
2022-07-03 12:20:57 +0800

    记录时间的另一种方式

---
+++ api/src/main/java/com/madm/learnroute/javaee/RecordTimePractice.java
@@ -0,0 +1,24 @@
+package com.madm.learnroute.javaee;
+
+import cn.hutool.core.date.StopWatch;
+
+/**
+ * 记录时间的另一个种方式
+ * @author dongming.ma
+ * @date 2022/7/3 12:14
+ */
+public class RecordTimePractice {
+    public static void main(String[] args) throws Exception {
+        StopWatch sw = new StopWatch();
+        sw.start("A");
+        Thread.sleep(500);
+        sw.stop();
+        sw.start("B");
+        Thread.sleep(300);
+        sw.stop();
+        sw.start("C");
+        Thread.sleep(200);
+        sw.stop();
+        System.out.println(sw.prettyPrint());
+    }
+}
