a5b51e5adf10389fe38b85856c70bcede0c8fbad
madongming <madongming1001@163.com>
2022-07-03 14:57:44 +0800

    测试守护线程不会影响jvm进程的退出 非守护线程会

--- api/src/main/java/com/madm/learnroute/javaee/ThreadDamonPractice.java
+++ api/src/main/java/com/madm/learnroute/javaee/ThreadDamonPractice.java
@@ -1,17 +1,26 @@
 package com.madm.learnroute.javaee;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadDamonPractice {
     public static void main(String[] args) {
         Thread mainThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 System.out.println("我是一个用户线程");
+                while (true) {
+                    try {
+                        TimeUnit.SECONDS.sleep(1L);
+                    } catch (InterruptedException e) {
+                    }
+                }
+
             }
         });
         Thread daemonThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                while(mainThread.isAlive()){
+                while (mainThread.isAlive()) {
                     System.out.println("我是一个守护线程");
                 }
             }
