160be82651fe6618c36d4a0ce374cb79505bc648
madongming <madongming1001@163.com>
2022-07-03 11:14:20 +0800

    查看各advice输出顺序

--- api/src/main/java/com/madm/learnroute/service/AspectA.java
+++ api/src/main/java/com/madm/learnroute/service/AspectA.java
@@ -14,30 +14,30 @@ public class AspectA {
         System.out.println("Before advice exec ...");
     }
 
-//    @Around("execution(public void com.madm.learnroute.service.CircularServiceA.methodA())")
-//    public void around(ProceedingJoinPoint pjp) throws Throwable {
-//        System.out.println("Around advice start exec ...");
-//        try {
-//            pjp.proceed();
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//        System.out.println("Around advice end exec ...");
-//    }
-//
-//    @After("execution(public void com.madm.learnroute.service.CircularServiceA.methodA())")
-//    public void after() {
-//        System.out.println("After advice exec ...");
-//    }
-//
-//    @AfterThrowing("execution(public void com.madm.learnroute.service.CircularServiceA.methodA())")
-//    public void afterThrowing() {
-//        System.out.println("AfterThrowing advice exec ...");
-//    }
-//
-//    @AfterReturning("execution(public void com.madm.learnroute.service.CircularServiceA.methodA())")
-//    public void afterReturning() {
-//        System.out.println("AfterReturning advice exec ...");
-//    }
+    @Around(value = "execution(public void com.madm.learnroute.service.CircularServiceA.methodA(..))")
+    public void around(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("Around advice start exec ...");
+        try {
+            pjp.proceed();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Around advice end exec ...");
+    }
+
+    @After(value = "execution(public void com.madm.learnroute.service.CircularServiceA.methodA(..)) && args(a, b)", argNames = "a,b")
+    public void after(String a, String b) {
+        System.out.println("After advice exec ...");
+    }
+
+    @AfterThrowing(value = "execution(public void com.madm.learnroute.service.CircularServiceA.methodA(..)) && args(a, b)", argNames = "a,b")
+    public void afterThrowing(String a, String b) {
+        System.out.println("AfterThrowing advice exec ...");
+    }
+
+    @AfterReturning(value = "execution(public void com.madm.learnroute.service.CircularServiceA.methodA(..)) && args(a, b)", argNames = "a,b")
+    public void afterReturning(String a, String b) {
+        System.out.println("AfterReturning advice exec ...");
+    }
 
 }
