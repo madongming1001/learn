28c12fe6437c8cb77d4adbeef84d016f6469b3a1
madongming <madongming1001@163.com>
2022-07-05 00:17:36 +0800

    面试题测试添加

---
+++ api/src/main/java/com/madm/learnroute/javaee/CreationObjectPractice.java
@@ -0,0 +1,58 @@
+package com.madm.learnroute.javaee;
+
+/**
+ * 创建对象的 4 种方法如下
+ * <p>
+ * 1、使用 new 关键字
+ * 2、反射机制
+ * 3、实现 Cloneable 接口，使用 clone 方法创建对象
+ * 4、序列化和反序列化
+ * 5、使用Constructor类的newInstance()方法创建对象
+ *
+ * JVM 对使用 new 方法创建对象的方式进行了优化，默认情况下，new 的效率更高。
+ * new 方式创建对象时，会调用类的构造函数。若构造函数中有耗时操作，则会影响 new 方法创建对象的效率。
+ * clone 方式创建对象，并不会调用类的构造函数。
+ *
+ * 参考文章：https://www.jianshu.com/p/18d329ec4c80
+ */
+public class CreationObjectPractice {
+
+    private static final int COUNT = 10000 * 1000;
+
+    /**
+     * new 和 clone 的方式效率对比
+     * @param args
+     * @throws CloneNotSupportedException
+     */
+    public static void main(String[] args) throws CloneNotSupportedException {
+        long s1 = System.currentTimeMillis();
+        for (int i = 0; i < COUNT; i++) {
+            Bean bean = new Bean("ylWang");
+        }
+
+        long s2 = System.currentTimeMillis();
+        Bean bean = new Bean("ylWang");
+        for (int i = 0; i < COUNT; i++) {
+            Bean b = bean.clone();
+        }
+
+        long s3 = System.currentTimeMillis();
+
+        System.out.println("new  = " + (s2 - s1));
+        System.out.println("clone = " + (s3 - s2));
+    }
+
+}
+
+class Bean implements Cloneable {
+    private String name;
+
+    public Bean(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Bean clone() throws CloneNotSupportedException {
+        return (Bean) super.clone();
+    }
+}
---
+++ api/src/main/java/com/madm/learnroute/javaee/ParadigmPractice.java
@@ -0,0 +1,31 @@
+package com.madm.learnroute.javaee;
+
+/**
+ * 泛型中 extends 和 super 的区别？
+ * <? extends T>和<? super T>是Java泛型中的“通配符（Wildcards）”和“边界（Bounds）”的概念。
+ *
+ * 表示类型的上界，表示参数化类型的可能是T 或是 T的子类
+ * <? extends T>：是指 “上界通配符（Upper Bounds Wildcards）” 不能往里存，只能往外取
+ *
+ * 表示类型下界（Java Core中叫超类型限定），表示参数化类型是此类型的超类型（父类型），直至Object
+ * <? super T>：是指 “下界通配符（Lower Bounds Wildcards）” 不影响往里存，但往外取只能放在Object对象里
+ *
+ * 参考文章：https://itimetraveler.github.io/2016/12/27/%E3%80%90Java%E3%80%91%E6%B3%9B%E5%9E%8B%E4%B8%AD%20extends%20%E5%92%8C%20super%20%E7%9A%84%E5%8C%BA%E5%88%AB%EF%BC%9F/
+ * @author dongming.ma
+ * @date 2022/7/4 23:56
+ */
+public class ParadigmPractice {
+    public static void main(String[] args) {
+        //编译器会认为装苹果的盘子 not is a 装水果的盘子
+        //所以，就算容器里装的东西之间有继承关系，但容器之间是没有继承关系的。所以我们不可以把Plate的引用传递给Plate。
+//        Plate<Fruit> p=new Plate<Apple>(new Apple());
+    }
+}
+class Fruit {}
+class Apple extends Fruit {}
+class Plate<T>{
+    private T item;
+    public Plate(T t){item=t;}
+    public void set(T t){item=t;}
+    public T get(){return item;}
+}
