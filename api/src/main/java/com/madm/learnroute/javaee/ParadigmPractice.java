5ce66ec0094339820cb9e53f0543bf604889bdd9
madongming <madongming1001@163.com>
2022-07-08 21:11:15 +0800

    追加笔记JAVA并发、利用SynchronousQueue实现线程通信

--- api/src/main/java/com/madm/learnroute/javaee/ParadigmPractice.java
+++ api/src/main/java/com/madm/learnroute/javaee/ParadigmPractice.java
@@ -1,16 +1,25 @@
 package com.madm.learnroute.javaee;
 
+import com.google.common.collect.Lists;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 泛型中 extends 和 super 的区别？
  * <? extends T>和<? super T>是Java泛型中的“通配符（Wildcards）”和“边界（Bounds）”的概念。
- *
+ * <p>
  * 表示类型的上界，表示参数化类型的可能是T 或是 T的子类
  * <? extends T>：是指 “上界通配符（Upper Bounds Wildcards）” 不能往里存，只能往外取，读取出来的东西只能存放在Fruit或它的基类里。
- *
+ * <p>
  * 表示类型下界（Java Core中叫超类型限定），表示参数化类型是此类型的超类型（父类型），直至Object
  * <? super T>：是指 “下界通配符（Lower Bounds Wildcards）” 不影响往里存，但往外取只能放在Object对象里，读取出来的东西只能存放在Object类里。
- *
+ * <p>
  * 参考文章：https://itimetraveler.github.io/2016/12/27/%E3%80%90Java%E3%80%91%E6%B3%9B%E5%9E%8B%E4%B8%AD%20extends%20%E5%92%8C%20super%20%E7%9A%84%E5%8C%BA%E5%88%AB%EF%BC%9F/
+ *
  * @author dongming.ma
  * @date 2022/7/4 23:56
  */
@@ -19,13 +28,44 @@ public class ParadigmPractice {
         //编译器会认为装苹果的盘子 not is a 装水果的盘子
         //所以，就算容器里装的东西之间有继承关系，但容器之间是没有继承关系的。所以我们不可以把Plate的引用传递给Plate。
 //        Plate<Fruit> p=new Plate<Apple>(new Apple());
+
+        Map<String, Fruit> map1 = new HashMap();
+        mapPrint1(map1);
+        Map<String, List<Fruit>> map2 = new HashMap();
+        List<Apple> plates = Lists.newArrayList(new Apple());
+//        map2.put("plates", plates);
+        mapPrint2(map2);
     }
+
+    public static void mapPrint1(Map<String, ? extends Fruit> map1) {
+        System.out.println(map1);
+    }
+
+    public static void mapPrint2(Map<String, ? extends List<? extends Fruit>> map2) {
+        System.out.println(map2);
+    }
+
 }
-class Fruit {}
-class Apple extends Fruit {}
-class Plate<T>{
+
+class Fruit {
+}
+
+class Apple extends Fruit {
+}
+
+@NoArgsConstructor
+class Plate<T> {
     private T item;
-    public Plate(T t){item=t;}
-    public void set(T t){item=t;}
-    public T get(){return item;}
+
+    public Plate(T t) {
+        item = t;
+    }
+
+    public void set(T t) {
+        item = t;
+    }
+
+    public T get() {
+        return item;
+    }
 }
--- api/src/main/java/com/madm/learnroute/jvm/synchronize/Juc03_Thread_Synchronize.java
+++ api/src/main/java/com/madm/learnroute/jvm/synchronize/Juc03_Thread_Synchronize.java
@@ -1,5 +1,7 @@
 package com.madm.learnroute.jvm.synchronize;
 
+import org.openjdk.jol.info.ClassLayout;
+
 /**
  *                  ,;,,;
  *                ,;;'(    社
@@ -14,24 +16,30 @@ package com.madm.learnroute.jvm.synchronize;
  * @date ：Created in 2020/4/28 17:15
  * @version: V1.0
  * @slogan: 天下风云出我辈，一入代码岁月催
- * @description: 
+ * @description:
+ * -XX:BiasedLockingStartupDelay=0 关闭偏向锁延迟
+ * -XX:-UseCompressedOops 对象头指针压缩
+ * -XX:-UseCompressedClassPointers klass pointer指针压缩
  **/
 public class Juc03_Thread_Synchronize {
 
-    private final static Object object = new Object();
-
-    public static void rl(){
+    public static void rl() throws Exception {
+        System.out.println(ClassLayout.parseInstance(new Object()).toPrintable());
+        //HotSpot 虚拟机在启动后有4s的延迟才会对每个新建的对象开启偏向锁模式
+        Thread.sleep(5000);
+        Object obj = new Object();
+        System.out.println(ClassLayout.parseInstance(obj).toPrintable());
         String tname = Thread.currentThread().getName();
-        synchronized (object) {
-            System.out.println(String.format("{}:) hold {}->monitor lock",tname,object));
-            synchronized (object){
-                System.out.println(String.format("{}:) re-hold {}->monitor lock",tname,object));
+        synchronized (obj) {
+            System.out.println(String.format("{}:) hold {}->monitor lock", tname, obj));
+            synchronized (obj) {
+                System.out.println(String.format("{}:) re-hold {}->monitor lock", tname, obj));
+                System.out.println(ClassLayout.parseInstance(obj).toPrintable());
             }
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws Exception {
         Juc03_Thread_Synchronize.rl();
     }
 }
