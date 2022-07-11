837a968c8f957b0e7388ea5d06c682c2c5af7e9a
madongming <madongming1001@163.com>
2022-07-11 12:31:52 +0800

    添加快速排序实现方式（单边循环）

--- algorithm/src/main/java/madm/interview_guide/QuickSortPractice.java
+++ algorithm/src/main/java/madm/interview_guide/QuickSortPractice.java
@@ -12,12 +12,19 @@ public class QuickSortPractice {
     public static void main(String[] args) {
 //        int[] ints = {6, 1, 2, 7, 9};
 //        int[] ints = {7, 6, 5, 3, 1, 4, 5, 6, 7};
-        int[] ints = {3,5,4,7};
-        quick_sort(ints, 0, ints.length - 1);
+        int[] ints = {3, 5, 4, 7};
+        quick_sort1(ints, 0, ints.length - 1);
         Arrays.stream(ints).forEach(System.out::println);
     }
 
-    private static void quick_sort(int[] arr, int begin, int end) {
+    /**
+     * 递归循环
+     *
+     * @param arr
+     * @param begin
+     * @param end
+     */
+    private static void quick_sort1(int[] arr, int begin, int end) {
         if (begin > end) {
             return;
         }
@@ -39,7 +46,77 @@ public class QuickSortPractice {
         }
         arr[begin] = arr[left];
         arr[left] = pivot;
-        quick_sort(arr, begin, left - 1);
-        quick_sort(arr, left + 1, end);
+        quick_sort1(arr, begin, left - 1);
+        quick_sort1(arr, left + 1, end);
+    }
+
+    /**
+     * 双边循环发
+     *
+     * @param arr
+     * @param startIndex
+     * @param endIndex
+     */
+    public static void quick_sort2(int[] arr, int startIndex, int endIndex) {
+        if (startIndex >= endIndex) {
+            return;
+        }
+        int pivotIndex = partition2(arr, startIndex, endIndex);
+        quick_sort2(arr, startIndex, pivotIndex - 1);
+        quick_sort1(arr, pivotIndex + 1, endIndex);
+    }
+
+    private static int partition2(int[] arr, int startIndex, int endIndex) {
+        int pivot = arr[startIndex];
+        int left = startIndex;
+        int right = endIndex;
+        while (left != right) {
+            while (arr[right] > pivot && left < right) {
+                right--;
+            }
+            while (arr[left] < pivot && left < right) {
+                left++;
+            }
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = left;
+            }
+        }
+        arr[startIndex] = arr[left];
+        arr[left] = pivot;
+        return left;
+    }
+
+    /**
+     * 单边循环法
+     *
+     * @param arr
+     * @param startIndex
+     * @param endIndex
+     */
+    public static void quick_sort3(int[] arr, int startIndex, int endIndex) {
+        if (startIndex >= endIndex) {
+            return;
+        }
+        int pivotIndex = partition3(arr, startIndex, endIndex);
+        quick_sort2(arr, startIndex, pivotIndex - 1);
+        quick_sort1(arr, pivotIndex + 1, endIndex);
+    }
+
+    private static int partition3(int[] arr, int startIndex, int endIndex) {
+        int pivot = arr[startIndex];
+        int mark = startIndex;
+        for (int i = startIndex + 1; i <= endIndex; i++) {
+            if (arr[i] < pivot) {
+                mark++;
+                int p = arr[mark];
+                arr[mark] = arr[i];
+                arr[i] = p;
+            }
+        }
+        arr[startIndex] = arr[mark];
+        arr[mark] = pivot;
+        return mark;
     }
 }
