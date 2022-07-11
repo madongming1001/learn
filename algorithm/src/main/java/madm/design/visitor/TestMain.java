97d9dd1b4ae3bd6d1a0c2c8a449dccb6ddfbaf6e
madongming <madongming1001@163.com>
2022-07-11 12:31:09 +0800

    修改测试类名称

---
+++ algorithm/src/main/java/madm/design/visitor/TestMain.java
@@ -0,0 +1,19 @@
+package madm.design.visitor;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class TestMain {
+    public static void main(String[] args) {
+        DataView dataView = new DataView();
+        log.info("\r\n家长视角访问：");
+        // 家长
+        dataView.show(new Parent());
+        log.info("\r\n校长视角访问：");
+        // 校长
+        dataView.show(new Principal());
+    }
+}
