1cf89f9d2b0262817873c4e04d9839ce8e1cda0e
madongming <madongming1001@163.com>
2022-07-22 23:44:37 +0800

    note compressed

--- api/src/main/java/com/madm/learnroute/javaee/ConcurrentLinkedQueuePractice.java
+++ api/src/main/java/com/madm/learnroute/javaee/ConcurrentLinkedQueuePractice.java
@@ -2,8 +2,7 @@ package com.madm.learnroute.javaee;
 
 public class ConcurrentLinkedQueuePractice {
     public static void main(String[] args) {
-        Long actualEnrollment = Long.parseLong("11");
-        actualEnrollment = actualEnrollment == null ? 0L : actualEnrollment;
+        Long actualEnrollment = Long.parseLong("11",2);
         System.out.println(actualEnrollment);
     }
 }
