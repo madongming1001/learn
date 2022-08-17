bc1441739e959b16f47d0fff9d3c44ac8021f18d
madongming <madongming1001@163.com>
2022-08-17 18:20:18 +0800

    补交Aug17

--- api/src/main/java/com/madm/learnroute/javaee/ObtainPlatformDefaultCharsetPractice.java
+++ api/src/main/java/com/madm/learnroute/javaee/ObtainPlatformDefaultCharsetPractice.java
@@ -1,6 +1,18 @@
-package com.madm.learnroute.javaee;/**
- * @author  dongming.ma
- * @date  2022/10/22 15:53
+package com.madm.learnroute.javaee;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author dongming.ma
+ * @date 2022/10/22 15:53
  */
 public class ObtainPlatformDefaultCharsetPractice {
+    public static void main(String[] args) {
+        //方法一
+        System.out.println(System.getProperty("file.encoding"));
+
+        //方法二
+        System.out.println(Charset.defaultCharset());
+        System.out.println(Integer.MAX_VALUE);
+    }
 }
