650956571c667a2f4689e6571c0663fb46196aa1
madongming <madongming1001@163.com>
2022-09-01 12:35:24 +0800

    补交Sep1

--- api/src/main/java/com/madm/learnroute/javaee/TwoDimensionalArraySum.java
+++ api/src/main/java/com/madm/learnroute/javaee/TwoDimensionalArraySum.java
@@ -1,5 +1,9 @@
 package com.madm.learnroute.javaee;
 
+/**
+ * 而为数组的第一个参数是宽 第二个参数是长
+ * 相当于是第一个参数个第二个参数
+ */
 public class TwoDimensionalArraySum {
     private static final int RUNS = 100;
     private static final int DIMENSION_1 = 1024 * 1024;//1048576
@@ -24,25 +28,28 @@ public class TwoDimensionalArraySum {
         long start = System.currentTimeMillis();
         for (int r = 0; r < RUNS; r++) {
             for (int i = 0; i < DIMENSION_1; i++) {//DIMENSION_1=1024*1024
-                for (int j=0;j<DIMENSION_2;j++){//6
-                    sum+=longs[i][j];
+                for (int j = 0; j < DIMENSION_2; j++) {//6
+                    sum += longs[i][j];
                 }
             }
         }
-        System.out.println("spend time1:"+(System.currentTimeMillis()-start));
-        System.out.println("sum1:"+sum);
+        System.out.println("spend time1:" + (System.currentTimeMillis() - start));
+        System.out.println("sum1:" + sum);
 
         sum = 0L;
         //符合空间局部性
         start = System.currentTimeMillis();
         for (int r = 0; r < RUNS; r++) {
-            for (int j=0;j<DIMENSION_2;j++) {//6
-                for (int i = 0; i < DIMENSION_1; i++){//1024*1024
-                    sum+=longs[i][j];
+            for (int j = 0; j < DIMENSION_2; j++) {//6
+                for (int i = 0; i < DIMENSION_1; i++) {//1024*1024
+                    sum += longs[i][j];
                 }
             }
         }
-        System.out.println("spend time2:"+(System.currentTimeMillis()-start));
-        System.out.println("sum2:"+sum);
+        System.out.println("spend time2:" + (System.currentTimeMillis() - start));
+        System.out.println("sum2:" + sum);
+
+        System.out.println(longs.length);
+        System.out.println(longs[0].length);
     }
 }
