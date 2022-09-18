e75448a5a32babbd89f87f75e3d4d769e66016e7
madongming <madongming1001@163.com>
2022-09-19 07:14:34 +0800

    补交Sep19

--- common/src/main/java/com/mdm/utils/UnsafeFactory.java
+++ common/src/main/java/com/mdm/utils/UnsafeFactory.java
@@ -1,43 +1,43 @@
-package com.mdm.utils;
-
-import sun.misc.Unsafe;
-
-import java.lang.reflect.Field;
-
-/**
- * @author Fox
- */
-public class UnsafeFactory {
-
-    /**
-     * 获取unsafe对象
-     *
-     * @return
-     */
-    public static Unsafe getUnsafe() {
-        try {
-            Field field = Unsafe.class.getDeclaredField("theUnsafe");
-            field.setAccessible(true);
-            return (Unsafe) field.get(null);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * 获取字段的内存偏移量
-     *
-     * @param unsafe
-     * @param clazz
-     * @param fieldName
-     * @return
-     */
-    public static long getFieldOffset(Unsafe unsafe, Class clazz, String fieldName) {
-        try {
-            return unsafe.objectFieldOffset(clazz.getDeclaredField(fieldName));
-        } catch (NoSuchFieldException e) {
-            throw new Error(e);
-        }
-    }
-}
+//package com.mdm.utils;
+//
+//import sun.misc.Unsafe;
+//
+//import java.lang.reflect.Field;
+//
+///**
+// * @author Fox
+// */
+//public class UnsafeFactory {
+//
+//    /**
+//     * 获取unsafe对象
+//     *
+//     * @return
+//     */
+//    public static Unsafe getUnsafe() {
+//        try {
+//            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+//            field.setAccessible(true);
+//            return (Unsafe) field.get(null);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    /**
+//     * 获取字段的内存偏移量
+//     *
+//     * @param unsafe
+//     * @param clazz
+//     * @param fieldName
+//     * @return
+//     */
+//    public static long getFieldOffset(Unsafe unsafe, Class clazz, String fieldName) {
+//        try {
+//            return unsafe.objectFieldOffset(clazz.getDeclaredField(fieldName));
+//        } catch (NoSuchFieldException e) {
+//            throw new Error(e);
+//        }
+//    }
+//}
