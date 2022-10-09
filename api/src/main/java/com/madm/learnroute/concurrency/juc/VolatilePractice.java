c925bfeb5e20311af5b5d1a066881fe7bfbbb06e
madongming <madongming1001@163.com>
2022-10-09 16:25:43 +0800

    补交Oct9

---
+++ api/src/main/java/com/madm/learnroute/concurrency/juc/VolatilePractice.java
@@ -0,0 +1,26 @@
+package com.madm.learnroute.concurrency.juc;
+
+public class VolatilePractice {
+
+    public static volatile int i = 0;
+    private static final int THREADS_COUNT = 20;
+
+    public static void increase() {
+        i++;
+    }
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREADS_COUNT];
+        for (int j = 0; j < threads.length; j++) {
+            threads[j] = new Thread(() -> {
+                for (int k = 0; k < 10000; k++) {
+                    increase();
+                }
+            });
+            threads[j].start();
+        }
+        while (Thread.activeCount() > 1)
+            Thread.yield();
+        System.out.println(i);
+    }
+}
