7c92d0babb4aae83fda5e52ff5c89e0e6b906615
madongming <madongming1001@163.com>
2022-10-19 22:55:34 +0800

    本地缓存caffeine咖啡因

--- common/src/main/java/com/mdm/utils/LocalCacheUtil.java
+++ common/src/main/java/com/mdm/utils/LocalCacheUtil.java
@@ -1,13 +1,16 @@
 package com.mdm.utils;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-public class GuavaCacheUtil {
+public class LocalCacheUtil {
 
     static Cache<String, String> myMap;
+    static com.github.benmanes.caffeine.cache.Cache<Object, Object> graphs;
 
     static {
          myMap = CacheBuilder.newBuilder()
@@ -17,6 +20,12 @@ public class GuavaCacheUtil {
                 .initialCapacity(100)
                 .maximumSize(1000)
                 .softValues()
+                .build();
+
+        graphs = Caffeine.newBuilder()
+                .maximumSize(10_000)
+                .expireAfterWrite(Duration.ofMinutes(5))
+                .refreshAfterWrite(Duration.ofMinutes(1))
                 .build();
     }
 }
