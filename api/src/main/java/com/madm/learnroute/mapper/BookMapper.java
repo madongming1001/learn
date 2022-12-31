0549b5d486b933f03243f7ece34efe7ca1d7c68a
madongming <madongming1001@163.com>
2022-12-31 11:21:34 +0800

    补交Dec31

--- api/src/main/java/com/madm/learnroute/mapper/BookMapper.java
+++ api/src/main/java/com/madm/learnroute/mapper/BookMapper.java
@@ -2,10 +2,12 @@ package com.madm.learnroute.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.madm.learnroute.model.Book;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author dongming.ma
  * @date 2022/11/9 22:05
  */
+@Mapper
 public interface BookMapper extends BaseMapper<Book> {
 }
