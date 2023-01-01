f6cd559080309c7c264219688bd6055e34990593
madongming <madongming1001@163.com>
2023-01-01 23:45:23 +0800

    补交Jan01

---
+++ api/src/main/java/com/madm/learnroute/technology/mybatis/plugin/MyBatisPlusPageConfig.java
@@ -0,0 +1,6 @@
+package com.madm.learnroute.technology.mybatis.plugin;/**
+ * @author  dongming.ma
+ * @date  2023/3/5 17:07
+ */
+public class MyBatisPlusPageConfig {
+}
---
+++ api/src/main/java/com/madm/learnroute/technology/spring/SpringBootStartupMonitor.java
@@ -0,0 +1,52 @@
+package com.madm.learnroute.technology.spring;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author dongming.ma
+ * @date 2023/3/5 11:24
+ */
+@Slf4j
+@NoArgsConstructor
+public class SpringBootStartupMonitor implements SpringApplicationRunListener {
+    @Override
+    public void starting() {
+        log.info("starting {}", LocalDateTime.now());
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableEnvironment environment) {
+        log.info("environmentPrepared {}", LocalDateTime.now());
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+        log.info("contextPrepared {}", LocalDateTime.now());
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+        log.info("contextLoaded {}", LocalDateTime.now());
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context) {
+        log.info("started {}", LocalDateTime.now());
+    }
+
+    @Override
+    public void running(ConfigurableApplicationContext context) {
+        log.info("running {}", LocalDateTime.now());
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+        log.info("failed {}", LocalDateTime.now());
+    }
+}
