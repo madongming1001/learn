5609837d50d99d8911f003fedfee27158bd0e895
madongming <madongming1001@163.com>
2023-01-03 10:45:21 +0800

    补交Jan03

--- api/src/main/java/com/madm/learnroute/service/AccountService.java
+++ api/src/main/java/com/madm/learnroute/service/AccountService.java
@@ -3,6 +3,8 @@ package com.madm.learnroute.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.madm.learnroute.model.Account;
 
+import java.util.List;
+
 /**
  * @author dongming.ma
  * @date 2022/11/9 22:07
@@ -12,4 +14,10 @@ public interface AccountService extends IService<Account> {
     int saveForJdbc(Account account);
 
     Account findAccountById(int id);
+
+    List<Account> findAll();
+
+    List<Account> findAllForCursor();
+
+    List<Account> findAllForCursorFetchSize();
 }
