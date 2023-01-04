586dfa68527b8d34e288c56ed68bf6d323f99c48
madongming <madongming1001@163.com>
2023-01-04 16:13:15 +0800

    补交Jan04

--- api/src/main/java/com/madm/learnroute/service/impl/AccountServiceImpl.java
+++ api/src/main/java/com/madm/learnroute/service/impl/AccountServiceImpl.java
@@ -1,22 +1,34 @@
 package com.madm.learnroute.service.impl;
 
+import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.hutool.extra.spring.SpringUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.madm.learnroute.mapper.AccountMapper;
 import com.madm.learnroute.model.Account;
 import com.madm.learnroute.service.AccountService;
-import org.apache.commons.lang3.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.shaded.com.google.common.collect.Lists;
+import org.apache.ibatis.cursor.Cursor;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
-import com.madm.learnroute.mapper.AccountMapper;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author dongming.ma
  * @date 2022/11/9 22:18
  */
 @Service
+@Slf4j
 public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> implements AccountService {
 
 
@@ -26,6 +38,9 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
     NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     @Autowired
     AccountMapper accountMapper;
+
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
 
     @Override
     public int saveForJdbc(Account account) {
@@ -41,5 +56,33 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
     public Account findAccountById(int id) {
         super.baseMapper.delete(Wrappers.<Account>lambdaQuery().eq(Account::getId, id));
         return accountMapper.selectOne(Wrappers.<Account>lambdaQuery().eq(Account::getId, id));
+    }
+
+    @Override
+    public List<Account> findAll() {
+        IPage<Account> accountPage = accountMapper.pageList(Page.of(10, 10), Wrappers.emptyWrapper());
+        return accountPage.getRecords();
+    }
+
+    @Override
+    public List<Account> findAllForCursor() {
+        List<Account> result = Lists.newArrayList();
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(); Cursor cursor = sqlSession.getMapper(AccountMapper.class).scan()) {
+            cursor.forEach(account -> result.add((Account) account));
+        } catch (Exception e) {
+            log.error("findAllForCursor method execution exception :{}", ExceptionUtil.getMessage(e));
+        }
+        return result;
+    }
+
+    @Override
+    public List<Account> findAllForCursorFetchSize() {
+        List<Account> result = Lists.newArrayList();
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(); Cursor cursor = sqlSession.getMapper(AccountMapper.class).scanFetchSize()) {
+            cursor.forEach(account -> result.add((Account) account));
+        } catch (IOException e) {
+            log.error("findAllForCursorFetchSize method execution exception :{}", ExceptionUtil.getMessage(e));
+        }
+        return result;
     }
 }
