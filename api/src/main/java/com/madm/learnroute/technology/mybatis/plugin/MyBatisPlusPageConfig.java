902fb7df46e145480e9f801597e88722cb82d9ca
madongming <madongming1001@163.com>
2023-01-05 08:45:24 +0800

    补交Jan05

--- api/src/main/java/com/madm/learnroute/technology/mybatis/plugin/MyBatisPlusPageConfig.java
+++ api/src/main/java/com/madm/learnroute/technology/mybatis/plugin/MyBatisPlusPageConfig.java
@@ -1,6 +1,27 @@
-package com.madm.learnroute.technology.mybatis.plugin;/**
- * @author  dongming.ma
- * @date  2023/3/5 17:07
+package com.madm.learnroute.technology.mybatis.plugin;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dongming.ma
+ * @date 2023/3/5 17:07
  */
+@Configuration
 public class MyBatisPlusPageConfig {
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除，以移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //乐观锁
+        //interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        //分页锁
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
 }
