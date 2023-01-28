0648e64dda165c68c9e31b35b645c24b289c0c80
madongming <madongming1001@163.com>
2023-01-28 23:13:35 +0800

    开始学习

--- api/src/main/java/com/madm/learnroute/javaee/concurrent/juc/threadpool/ThreadPoolExecutorPractice.java
+++ api/src/main/java/com/madm/learnroute/javaee/concurrent/juc/threadpool/ThreadPoolExecutorPractice.java
@@ -30,18 +30,18 @@ public class ThreadPoolExecutorPractice {
 
     @SneakyThrows
     public static void main(String[] args) {
-//        ExecutorService executorService = Executors.newFixedThreadPool(1);
-//
-//        executorService.submit(() -> {
-//            System.out.println(Thread.currentThread().getName());
-//            int i = 1 / 0;
-//        });
-//
-//        Thread.sleep(2000L);
-//        executorService.submit(() -> {
-//            System.out.println(Thread.currentThread().getName());
-//            System.out.println("当线程池抛出异常后继续新的任务");
-//        });
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        executorService.submit(() -> {
+            System.out.println(Thread.currentThread().getName());
+            int i = 1 / 0;
+        });
+
+        Thread.sleep(2000L);
+        executorService.submit(() -> {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println("当线程池抛出异常后继续新的任务");
+        });
 
         executeSchedule();
         threadPoolParameterTest();
