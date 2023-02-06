b4da018b8ebc901e48558fdcc37a1dc6980b848b
madongming <madongming1001@163.com>
2023-02-06 15:32:38 +0800

    补交Feb06

--- api/src/main/java/com/madm/learnroute/config/ThreadPoolConfig.java
+++ api/src/main/java/com/madm/learnroute/config/ThreadPoolConfig.java
@@ -1,6 +1,21 @@
-package com.madm.learnroute.config;/**
- * @author  dongming.ma
- * @date  2023/2/18 11:20
+package com.madm.learnroute.config;
+
+import com.madm.learnroute.common.ThreadPoolExecutorMdcWrapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author dongming.ma
+ * @date 2023/2/18 11:20
  */
+@Configuration
 public class ThreadPoolConfig {
+
+    @Bean
+    public Executor requestExecutor() {
+        ThreadPoolExecutorMdcWrapper threadPool = new ThreadPoolExecutorMdcWrapper("asyncCustomThread");
+        return threadPool;
+    }
 }
