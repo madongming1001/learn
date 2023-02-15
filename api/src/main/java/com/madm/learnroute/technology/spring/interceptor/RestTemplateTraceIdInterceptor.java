c9322206a9183362b5a1d0450afd817dcac6fa38
madongming <madongming1001@163.com>
2023-02-16 04:32:23 +0800

    补交Feb16

--- api/src/main/java/com/madm/learnroute/technology/spring/interceptor/RestTemplateTraceIdInterceptor.java
+++ api/src/main/java/com/madm/learnroute/technology/spring/interceptor/RestTemplateTraceIdInterceptor.java
@@ -1,6 +1,29 @@
-package com.madm.learnroute.technology.spring.interceptor;/**
- * @author  dongming.ma
- * @date  2023/2/19 15:32
+package com.madm.learnroute.technology.spring.interceptor;
+
+import com.madm.learnroute.constant.TraceLogEnum;
+import org.slf4j.MDC;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+/**
+ * @author dongming.ma
+ * @date 2023/2/19 15:32
  */
-public class RestTemplateTraceIdInterceptor {
+public class RestTemplateTraceIdInterceptor implements ClientHttpRequestInterceptor {
+
+    //restTemplate.setInterceptors(Arrays.asList(new RestTemplateTraceIdInterceptor()));
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        String traceId = MDC.get(TraceLogEnum.TRACE_LOG_ID.getValue());
+        if (traceId != null) {
+            request.getHeaders().add(TraceLogEnum.TRACE_LOG_ID.getValue(), traceId);
+        }
+
+        return execution.execute(request, body);
+    }
 }
