05293edfceda07d0de9cbffc7f00a4ce2759376f
madongming <madongming1001@163.com>
2023-02-18 14:23:20 +0800

    补交Feb18

--- api/src/main/java/com/madm/learnroute/feign/FeignPreCallHandle.java
+++ api/src/main/java/com/madm/learnroute/feign/FeignPreCallHandle.java
@@ -27,7 +27,6 @@ public class FeignPreCallHandle implements RequestInterceptor {
         String dispatcherURI = rs.url();
         String traceId = ThreadLocalHolder.getScene();
         log.info("set traceId: requestURI[{}], dispatcherURI[{}], traceId[{}]", requestURI, dispatcherURI, traceId);
-        rs.path();
     }
 
 }
