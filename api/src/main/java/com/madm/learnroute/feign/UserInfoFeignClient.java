a9a613f18989dead70056cf524c954847f4f9e54
madongming <madongming1001@163.com>
2023-02-19 16:23:08 +0800

    openfeign请求改为okhttpclient方式

--- api/src/main/java/com/madm/learnroute/feign/UserInfoFeignClient.java
+++ api/src/main/java/com/madm/learnroute/feign/UserInfoFeignClient.java
@@ -3,16 +3,19 @@ package com.madm.learnroute.feign;
 import com.mdm.model.Response;
 import com.madm.learnroute.feign.callback.UserInfoCallbackFactory;
 import com.mdm.pojo.User;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@RefreshScope
 @FeignClient(name = "learn-api", url = "${feign.server.nacosconfig}", fallbackFactory = UserInfoCallbackFactory.class)
 @Component
 public interface UserInfoFeignClient {
 
-    @PostMapping("practice/getNacosConfig")
+    @GetMapping("practice/getNacosConfig")
     Response getNacosConfig();
 
     @PostMapping("practice/protobufShow")
