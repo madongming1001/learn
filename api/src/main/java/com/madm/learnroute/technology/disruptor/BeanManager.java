148bec57d1c9c54f3d50d29b07c47bfd2e73d46f
madongming <madongming1001@163.com>
2023-03-02 23:07:24 +0800

    spring实现策略模式

--- api/src/main/java/com/madm/learnroute/technology/disruptor/BeanManager.java
+++ api/src/main/java/com/madm/learnroute/technology/disruptor/BeanManager.java
@@ -1,9 +1,14 @@
 package com.madm.learnroute.technology.disruptor;
 
+import com.madm.learnroute.service.AccountService;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 获取实例化对象
@@ -12,9 +17,11 @@ import org.springframework.stereotype.Component;
  * @date 2023/1/18 17:52
  */
 @Component
-public class BeanManager implements ApplicationContextAware {
+public class BeanManager implements ApplicationContextAware, InitializingBean {
 
-    private static ApplicationContext applicationContext = null;
+    private static ApplicationContext applicationContext;
+
+    private final Map<Integer, AccountService> handlerMap = new HashMap<>();
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -32,4 +39,21 @@ public class BeanManager implements ApplicationContextAware {
     public static <T> T getBean(Class<T> clazz) {
         return applicationContext.getBean(clazz);
     }
+
+    @Override
+    public void afterPropertiesSet() {
+        Map<String, AccountService> beanMap = applicationContext.getBeansOfType(AccountService.class);
+        beanMap.forEach((k, v) -> {
+            handlerMap.put(beanMap.get(k).hashCode(), v);
+        });
+    }
+
+    public AccountService getHandler(Integer type) {
+        if (handlerMap.containsKey(type)) {
+            return handlerMap.get(type);
+        }
+        return null;
+    }
+
+
 }
