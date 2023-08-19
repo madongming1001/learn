ce53dea37b412025a74019c5d1eeff900cd5220c
madongming <madongming1001@163.com>
2023-08-19 12:04:31 +0800

    补交Aug19

--- api/src/main/java/com/madm/learnroute/jvm/jmm/tmodel/ThreadModel.java
+++ api/src/main/java/com/madm/learnroute/jvm/jmm/tmodel/ThreadModel.java
@@ -12,15 +12,12 @@ public class ThreadModel {
     public static void main(String[] args) {
 
         for (int i = 0; i < 200; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (true) {
-                        try {
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
             }).start();
