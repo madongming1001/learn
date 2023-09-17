847f9181bc72d4a3ec333c009dbefaefb432613a
madongming <madongming1001@163.com>
2023-09-17 16:46:40 +0800

    吃饭睡觉打豆豆

--- algorithm/src/main/java/com/madm/data_structure/SkipList.java
+++ algorithm/src/main/java/com/madm/data_structure/SkipList.java
@@ -1,298 +1,248 @@
 package com.madm.data_structure;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 public class SkipList {
 
-    private static final int MAX_LEVEL = 16;
-    private int levelCount = 1;
+    // 跳表的节点定义
+    public static class SkipListNode<K extends Comparable<K>, V> {
+        public K key;
+        public V val;
+        public ArrayList<SkipListNode<K, V>> nextNodes;
 
-    /**
-     * 带头链表
-     */
-    private Node head = new Node(MAX_LEVEL);
-    private Random r = new Random();
+        public SkipListNode(K k, V v) {
+            key = k;
+            val = v;
+            nextNodes = new ArrayList<>();
+        }
+
+        // 遍历的时候，如果是往右遍历到的null(next == null), 遍历结束
+        // 头(null), 头节点的null，认为最小
+        // node  -> 头，node(null, "")  node.isKeyLess(!null)  true
+        // node里面的key是否比otherKey小，true，不是false
+        public boolean isKeyLess(K otherKey) {
+            //  otherKey == null -> false
+            return otherKey != null && (key == null || key.compareTo(otherKey) < 0);
+        }
+
+        public boolean isKeyEqual(K otherKey) {
+            return (key == null && otherKey == null)
+                    || (key != null && otherKey != null && key.compareTo(otherKey) == 0);
+        }
+
+    }
+
+    public static class SkipListMap<K extends Comparable<K>, V> {
+        private static final double PROBABILITY = 0.5; // < 0.5 继续做，>=0.5 停
+        private SkipListNode<K, V> head;
+        private int size;
+        private int maxLevel;
+
+        public SkipListMap() {
+            head = new SkipListNode<>(null, null);
+            head.nextNodes.add(null); // 0
+            size = 0;
+            maxLevel = 0;
+        }
+
+        // 从最高层开始，一路找下去，
+        // 最终，找到第0层的<key的最右的节点
+        private SkipListNode<K, V> mostRightLessNodeInTree(K key) {
+            if (key == null) {
+                return null;
+            }
+            int level = maxLevel;
+            SkipListNode<K, V> cur = head;
+            while (level >= 0) { // 从上层跳下层
+                //  cur  level  -> level-1
+                cur = mostRightLessNodeInLevel(key, cur, level--);
+            }
+            return cur;
+        }
+
+        // 在level层里，如何往右移动
+        // 现在来到的节点是cur，来到了cur的level层，在level层上，找到<key最后一个节点并返回
+        private SkipListNode<K, V> mostRightLessNodeInLevel(K key,
+                                                            SkipListNode<K, V> cur,
+                                                            int level) {
+            SkipListNode<K, V> next = cur.nextNodes.get(level);
+            while (next != null && next.isKeyLess(key)) {
+                cur = next;
+                next = cur.nextNodes.get(level);
+            }
+            return cur;
+        }
+
+        public boolean containsKey(K key) {
+            if (key == null) {
+                return false;
+            }
+            SkipListNode<K, V> less = mostRightLessNodeInTree(key);
+            SkipListNode<K, V> next = less.nextNodes.get(0);
+            return next != null && next.isKeyEqual(key);
+        }
+
+        // 新增、改value
+        public void put(K key, V value) {
+            if (key == null) {
+                return;
+            }
+            // 0层上，最右一个，< key 的Node -> >key
+            SkipListNode<K, V> less = mostRightLessNodeInTree(key);
+            SkipListNode<K, V> find = less.nextNodes.get(0);
+            if (find != null && find.isKeyEqual(key)) {
+                find.val = value;
+            } else { // find == null   8   7   9
+                size++;
+                int newNodeLevel = 0;
+                while (Math.random() < PROBABILITY) {
+                    newNodeLevel++;
+                }
+                // newNodeLevel
+                while (newNodeLevel > maxLevel) {
+                    head.nextNodes.add(null);
+                    maxLevel++;
+                }
+                SkipListNode<K, V> newNode = new SkipListNode<K, V>(key, value);
+                for (int i = 0; i <= newNodeLevel; i++) {
+                    newNode.nextNodes.add(null);
+                }
+                int level = maxLevel;
+                SkipListNode<K, V> pre = head;
+                while (level >= 0) {
+                    // level 层中，找到最右的 < key 的节点
+                    pre = mostRightLessNodeInLevel(key, pre, level);
+                    if (level <= newNodeLevel) {
+                        newNode.nextNodes.set(level, pre.nextNodes.get(level));
+                        pre.nextNodes.set(level, newNode);
+                    }
+                    level--;
+                }
+            }
+        }
+
+        public V get(K key) {
+            if (key == null) {
+                return null;
+            }
+            SkipListNode<K, V> less = mostRightLessNodeInTree(key);
+            SkipListNode<K, V> next = less.nextNodes.get(0);
+            return next != null && next.isKeyEqual(key) ? next.val : null;
+        }
+
+        public void remove(K key) {
+            if (containsKey(key)) {
+                size--;
+                int level = maxLevel;
+                SkipListNode<K, V> pre = head;
+                while (level >= 0) {
+                    pre = mostRightLessNodeInLevel(key, pre, level);
+                    SkipListNode<K, V> next = pre.nextNodes.get(level);
+                    // 1）在这一层中，pre下一个就是key
+                    // 2）在这一层中，pre的下一个key是>要删除key
+                    if (next != null && next.isKeyEqual(key)) {
+                        // free delete node memory -> C++
+                        // level : pre -> next(key) -> ...
+                        pre.nextNodes.set(level, next.nextNodes.get(level));
+                    }
+                    // 在level层只有一个节点了，就是默认节点head
+                    if (level != 0 && pre == head && pre.nextNodes.get(level) == null) {
+                        head.nextNodes.remove(level);
+                        maxLevel--;
+                    }
+                    level--;
+                }
+            }
+        }
+
+        public K firstKey() {
+            return head.nextNodes.get(0) != null ? head.nextNodes.get(0).key : null;
+        }
+
+        public K lastKey() {
+            int level = maxLevel;
+            SkipListNode<K, V> cur = head;
+            while (level >= 0) {
+                SkipListNode<K, V> next = cur.nextNodes.get(level);
+                while (next != null) {
+                    cur = next;
+                    next = cur.nextNodes.get(level);
+                }
+                level--;
+            }
+            return cur.key;
+        }
+
+        public K ceilingKey(K key) {
+            if (key == null) {
+                return null;
+            }
+            SkipListNode<K, V> less = mostRightLessNodeInTree(key);
+            SkipListNode<K, V> next = less.nextNodes.get(0);
+            return next != null ? next.key : null;
+        }
+
+        public K floorKey(K key) {
+            if (key == null) {
+                return null;
+            }
+            SkipListNode<K, V> less = mostRightLessNodeInTree(key);
+            SkipListNode<K, V> next = less.nextNodes.get(0);
+            return next != null && next.isKeyEqual(key) ? next.key : less.key;
+        }
+
+        public int size() {
+            return size;
+        }
+
+    }
+
+    // for test
+    public static void printAll(SkipListMap<String, String> obj) {
+        for (int i = obj.maxLevel; i >= 0; i--) {
+            System.out.print("Level " + i + " : ");
+            SkipListNode<String, String> cur = obj.head;
+            while (cur.nextNodes.get(i) != null) {
+                SkipListNode<String, String> next = cur.nextNodes.get(i);
+                System.out.print("(" + next.key + " , " + next.val + ") ");
+                cur = next;
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        SkipList list = new SkipList();
-        list.insert(1, 3);
-        list.insert(2, 3);
-        list.insert(3, 2);
-        list.insert(4, 4);
-        list.insert(5, 10);
-        list.insert(6, 4);
-        list.insert(8, 5);
-        list.insert(7, 4);
-        list.printAll_beautiful();
-        list.printAll();
-        /**
-         * 结果如下：
-         * 									    null:15-------
-         * 									    null:14-------
-         * 									    null:13-------
-         * 									    null:12-------
-         * 									    null:11-------
-         * 									    null:10-------
-         * 										   5:9-------
-         * 										   5:8-------
-         * 										   5:7-------
-         * 										   5:6-------
-         * 										   5:5-------
-         * 										   5:4-------					 8:4-------
-         * 							     4:3-------5:3-------6:3-------7:3-------8:3-------
-         * 1:2-------2:2-------		     4:2-------5:2-------6:2-------7:2-------8:2-------
-         * 1:1-------2:1-------3:1-------4:1-------5:1-------6:1-------7:1-------8:1-------
-         * 1:0-------2:0-------3:0-------4:0-------5:0-------6:0-------7:0-------8:0-------
-         * { data: 1; levels: 3 } { data: 2; levels: 3 } { data: 3; levels: 2 } { data: 4; levels: 4 }
-         * { data: 5; levels: 10 } { data: 6; levels: 4 } { data: 7; levels: 4 } { data: 8; levels: 5 }
-         */
-        // 优化后insert()
+        SkipListMap<String, String> test = new SkipListMap<>();
+        printAll(test);
+        System.out.println("======================");
+        test.put("A", "10");
+        printAll(test);
+        System.out.println("======================");
+        test.remove("A");
+        printAll(test);
+        System.out.println("======================");
+        test.put("E", "E");
+        test.put("B", "B");
+        test.put("A", "A");
+        test.put("F", "F");
+        test.put("C", "C");
+        test.put("D", "D");
+        printAll(test);
+        System.out.println("======================");
+        System.out.println(test.containsKey("B"));
+        System.out.println(test.containsKey("Z"));
+        System.out.println(test.firstKey());
+        System.out.println(test.lastKey());
+        System.out.println(test.floorKey("D"));
+        System.out.println(test.ceilingKey("D"));
+        System.out.println("======================");
+        test.remove("D");
+        printAll(test);
+        System.out.println("======================");
+        System.out.println(test.floorKey("D"));
+        System.out.println(test.ceilingKey("D"));
 
-        SkipList list2 = new SkipList();
-        list2.add(1);
-        list2.add(2);
-        list2.add(6);
-        list2.add(7);
-        list2.add(8);
-        list2.add(3);
-        list2.add(4);
-        list2.add(5);
-        System.out.println();
-        list2.printAll_beautiful();
-    }
-
-    public Node find(int value) {
-        Node p = head;
-        // 从最大层开始查找，找到前一节点，通过--i，移动到下层再开始查找
-        for (int i = levelCount - 1; i >= 0; --i) {
-            while (p.forwards[i] != null && p.forwards[i].data < value) {
-                // 找到前一节点
-                p = p.forwards[i];
-            }
-        }
-
-        if (p.forwards[0] != null && p.forwards[0].data == value) {
-            return p.forwards[0];
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * 优化了作者zheng的插入方法
-     *
-     * @param value 值
-     */
-    public void insert(int value) {
-        int level = head.forwards[0] == null ? 1 : randomLevel();
-        // 每次只增加一层，如果条件满足
-        if (level > levelCount) {
-            level = ++levelCount;
-        }
-        Node newNode = new Node(level);
-        newNode.data = value;
-        Node update[] = new Node[level];
-        for (int i = 0; i < level; ++i) {
-            update[i] = head;
-        }
-
-        Node p = head;
-        // 从最大层开始查找，找到前一节点，通过--i，移动到下层再开始查找
-        for (int i = levelCount - 1; i >= 0; --i) {
-            while (p.forwards[i] != null && p.forwards[i].data < value) {
-                // 找到前一节点
-                p = p.forwards[i];
-            }
-            // levelCount 会 > level，所以加上判断
-            if (level > i) {
-                update[i] = p;
-            }
-
-        }
-        for (int i = 0; i < level; ++i) {
-            newNode.forwards[i] = update[i].forwards[i];
-            update[i].forwards[i] = newNode;
-        }
 
     }
 
-    /**
-     * 优化了作者zheng的插入方法2
-     *
-     * @param value 值
-     */
-    public void add(int value) {
-        int level = head.forwards[0] == null ? 1 : randomLevel();
-        // 每次只增加一层，如果条件满足
-        if (level > levelCount) {
-            level = ++levelCount;
-        }
-        Node newNode = new Node(level);
-        newNode.data = value;
-        Node p = head;
-        // 从最大层开始查找，找到前一节点，通过--i，移动到下层再开始查找
-        for (int i = levelCount - 1; i >= 0; --i) {
-            while (p.forwards[i] != null && p.forwards[i].data < value) {
-                // 找到前一节点
-                p = p.forwards[i];
-            }
-            // levelCount 会 > level，所以加上判断
-            if (level > i) {
-                if (p.forwards[i] == null) {
-                    p.forwards[i] = newNode;
-                } else {
-                    Node next = p.forwards[i];
-                    p.forwards[i] = newNode;
-                    newNode.forwards[i] = next;
-                }
-            }
-
-        }
-
-    }
-
-    /**
-     * 作者zheng的插入方法，未优化前，优化后参见上面insert()
-     *
-     * @param value
-     * @param level 0 表示随机层数，不为0，表示指定层数，指定层数
-     *              可以让每次打印结果不变动，这里是为了便于学习理解
-     */
-    public void insert(int value, int level) {
-        // 随机一个层数
-        if (level == 0) {
-            level = randomLevel();
-        }
-        // 创建新节点
-        Node newNode = new Node(level);
-        newNode.data = value;
-        // 表示从最大层到低层，都要有节点数据
-        newNode.maxLevel = level;
-        // 记录要更新的层数，表示新节点要更新到哪几层
-        Node update[] = new Node[level];
-        for (int i = 0; i < level; ++i) {
-            update[i] = head;
-        }
-
-        /**
-         *
-         * 1，说明：层是从下到上的，这里最下层编号是0，最上层编号是15
-         * 2，这里没有从已有数据最大层（编号最大）开始找，（而是随机层的最大层）导致有些问题。
-         *    如果数据量为1亿，随机level=1 ，那么插入时间复杂度为O（n）
-         */
-        Node p = head;
-        for (int i = level - 1; i >= 0; --i) {
-            while (p.forwards[i] != null && p.forwards[i].data < value) {
-                p = p.forwards[i];
-            }
-            // 这里update[i]表示当前层节点的前一节点，因为要找到前一节点，才好插入数据
-            update[i] = p;
-        }
-
-        // 将每一层节点和后面节点关联
-        for (int i = 0; i < level; ++i) {
-            // 记录当前层节点后面节点指针
-            newNode.forwards[i] = update[i].forwards[i];
-            // 前一个节点的指针，指向当前节点
-            update[i].forwards[i] = newNode;
-        }
-
-        // 更新层高
-        if (levelCount < level) levelCount = level;
-    }
-
-    public void delete(int value) {
-        Node[] update = new Node[levelCount];
-        Node p = head;
-        for (int i = levelCount - 1; i >= 0; --i) {
-            while (p.forwards[i] != null && p.forwards[i].data < value) {
-                p = p.forwards[i];
-            }
-            update[i] = p;
-        }
-
-        if (p.forwards[0] != null && p.forwards[0].data == value) {
-            for (int i = levelCount - 1; i >= 0; --i) {
-                if (update[i].forwards[i] != null && update[i].forwards[i].data == value) {
-                    update[i].forwards[i] = update[i].forwards[i].forwards[i];
-                }
-            }
-        }
-    }
-
-    /**
-     * 随机 level 次，如果是奇数层数 +1，防止伪随机
-     *
-     * @return
-     */
-    private int randomLevel() {
-        int level = 1;
-        for (int i = 1; i < MAX_LEVEL; ++i) {
-            if (r.nextInt() % 2 == 1) {
-                level++;
-            }
-        }
-        return level;
-    }
-
-    /**
-     * 打印每个节点数据和最大层数
-     */
-    public void printAll() {
-        Node p = head;
-        while (p.forwards[0] != null) {
-            System.out.print(p.forwards[0] + " ");
-            p = p.forwards[0];
-        }
-        System.out.println();
-    }
-
-    /**
-     * 打印所有数据
-     */
-    public void printAll_beautiful() {
-        Node p = head;
-        Node[] c = p.forwards;
-        Node[] d = c;
-        int maxLevel = c.length;
-        for (int i = maxLevel - 1; i >= 0; i--) {
-            do {
-                System.out.print((d[i] != null ? d[i].data : null) + ":" + i + "-------");
-            } while (d[i] != null && (d = d[i].forwards)[i] != null);
-            System.out.println();
-            d = c;
-        }
-    }
-
-    /**
-     * 跳表的节点，每个节点记录了当前节点数据和所在层数数据
-     */
-    public class Node {
-        private int data = -1;
-        /**
-         * 表示当前节点位置的下一个节点所有层的数据，从上层切换到下层，就是数组下标-1，
-         * forwards[3]表示当前节点在第三层的下一个节点。
-         */
-        private Node forwards[];
-
-        /**
-         * 这个值其实可以不用，看优化insert()
-         */
-        private int maxLevel = 0;
-
-        public Node(int level) {
-            forwards = new Node[level];
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder builder = new StringBuilder();
-            builder.append("{ data: ");
-            builder.append(data);
-            builder.append("; levels: ");
-            builder.append(maxLevel);
-            builder.append(" }");
-            return builder.toString();
-        }
-    }
 }
